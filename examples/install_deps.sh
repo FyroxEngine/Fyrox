1e4a5ee1a456c956d6f96ee6160b51c8f47489bc
Aleks Pickle <aleks.work2222@gmail.com>
2020-10-09 15:18:26 +0300

    deps: add bash script to install latest rg3d components

---
+++ examples/install_deps.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "Download latest rg3d-ui..."
+git clone https://github.com/mrDIMAS/rg3d-ui ../rg3d-ui
+
+echo "Download latest rg3d-core..."
+git clone https://github.com/mrDIMAS/rg3d-core ../rg3d-core
+
+echo "Download latest rg3d-sound..."
+git clone https://github.com/mrDIMAS/rg3d-sound ../rg3d-sound
+
+echo "Download latest rg3d-physics..."
+git clone https://github.com/mrDIMAS/rg3d-physics ../rg3d-physics
+
+echo "Done."
