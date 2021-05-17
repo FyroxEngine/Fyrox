67284c8668f8d3458bb8433f5dac9a5c7362ad2c
Dmitry <d1maxa@yandex.ru>
2021-05-17 09:05:34 +0300

    moved texture and geometry caches into separate module

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -15,6 +15,7 @@ pub mod surface;
 
 mod batch;
 mod blur;
+mod cache;
 mod deferred_light_renderer;
 mod flat_shader;
 mod forward_renderer;
@@ -27,60 +28,55 @@ mod sprite_renderer;
 mod ssao;
 mod ui_renderer;
 
-use crate::renderer::renderer2d::Renderer2d;
-use crate::scene2d::Scene2dContainer;
 use crate::{
-    core::instant,
     core::{
         algebra::{Matrix4, Vector2, Vector3},
         color::Color,
+        instant,
         math::Rect,
         pool::Handle,
         scope_profile,
     },
     engine::resource_manager::TimedEntry,
     gui::{draw::DrawingContext, message::MessageData, Control, UserInterface},
-    renderer::framework::{
-        error::FrameworkError,
-        framebuffer::{Attachment, AttachmentKind, CullFace, DrawParameters, FrameBuffer},
-        geometry_buffer::{
-            AttributeDefinition, AttributeKind, BufferBuilder, DrawCallStatistics, ElementKind,
-            GeometryBuffer, GeometryBufferBuilder, GeometryBufferKind,
-        },
-        gpu_texture::{
-            Coordinate, GpuTexture, GpuTextureKind, MagnificationFilter, MinificationFilter,
-            PixelKind,
-        },
-        state::{PipelineState, PipelineStatistics},
-    },
     renderer::{
-        batch::{BatchStorage, InstanceData},
+        batch::BatchStorage,
+        cache::{GeometryCache, TextureCache},
         debug_renderer::DebugRenderer,
         deferred_light_renderer::{
             DeferredLightRenderer, DeferredRendererContext, LightingStatistics,
         },
         flat_shader::FlatShader,
         forward_renderer::{ForwardRenderContext, ForwardRenderer},
+        framework::{
+            error::FrameworkError,
+            framebuffer::{Attachment, AttachmentKind, CullFace, DrawParameters, FrameBuffer},
+            geometry_buffer::DrawCallStatistics,
+            gpu_texture::{
+                GpuTexture, GpuTextureKind, MagnificationFilter, MinificationFilter, PixelKind,
+            },
+            state::{PipelineState, PipelineStatistics},
+        },
         fxaa::FxaaRenderer,
         gbuffer::{GBuffer, GBufferRenderContext},
         particle_system_renderer::{ParticleSystemRenderContext, ParticleSystemRenderer},
+        renderer2d::Renderer2d,
         sprite_renderer::{SpriteRenderContext, SpriteRenderer},
         surface::SurfaceSharedData,
         ui_renderer::{UiRenderContext, UiRenderer},
     },
-    resource::texture::{Texture, TextureKind, TextureState},
+    resource::texture::{Texture, TextureKind},
     scene::{node::Node, Scene, SceneContainer},
-    utils::log::{Log, MessageKind},
+    scene2d::Scene2dContainer,
 };
 #[cfg(feature = "serde_integration")]
 use serde::{Deserialize, Serialize};
-use std::sync::mpsc::{Receiver, Sender};
 use std::{
     cell::RefCell,
     collections::{hash_map::Entry, HashMap},
     fmt::{Display, Formatter},
-    ops::Deref,
     rc::Rc,
+    sync::mpsc::{Receiver, Sender},
 };
 
 /// Renderer statistics for one frame, also includes current frames per second
@@ -460,249 +456,6 @@ pub struct Renderer {
     // MUST BE LAST! Otherwise you'll get crash, because other parts of the renderer will
     // contain **pointer** to pipeline state. It must be dropped last!
     state: Box<PipelineState>,
-}
-
-#[derive(Default)]
-pub(in crate) struct GeometryCache {
-    map: HashMap<usize, TimedEntry<GeometryBuffer>>,
-}
-
-impl GeometryCache {
-    fn get(&mut self, state: &mut PipelineState, data: &SurfaceSharedData) -> &mut GeometryBuffer {
-        scope_profile!();
-
-        let key = (data as *const _) as usize;
-
-        let geometry_buffer = self.map.entry(key).or_insert_with(|| {
-            let geometry_buffer = GeometryBufferBuilder::new(ElementKind::Triangle)
-                .with_buffer_builder(
-                    BufferBuilder::new(
-                        GeometryBufferKind::StaticDraw,
-                        Some(data.vertices.as_slice()),
-                    )
-                    .with_attribute(AttributeDefinition {
-                        location: 0,
-                        divisor: 0,
-                        kind: AttributeKind::Float3,
-                        normalized: false,
-                    })
-                    .with_attribute(AttributeDefinition {
-                        location: 1,
-                        divisor: 0,
-                        kind: AttributeKind::Float2,
-                        normalized: false,
-                    })
-                    .with_attribute(AttributeDefinition {
-                        location: 2,
-                        divisor: 0,
-                        kind: AttributeKind::Float2,
-                        normalized: false,
-                    })
-                    .with_attribute(AttributeDefinition {
-                        location: 3,
-                        divisor: 0,
-                        kind: AttributeKind::Float3,
-                        normalized: false,
-                    })
-                    .with_attribute(AttributeDefinition {
-                        location: 4,
-                        divisor: 0,
-                        kind: AttributeKind::Float4,
-                        normalized: false,
-                    })
-                    .with_attribute(AttributeDefinition {
-                        location: 5,
-                        divisor: 0,
-                        kind: AttributeKind::Float4,
-                        normalized: false,
-                    })
-                    .with_attribute(AttributeDefinition {
-                        location: 6,
-                        divisor: 0,
-                        kind: AttributeKind::UnsignedByte4,
-                        normalized: false,
-                    }),
-                )
-                // Buffer for world and world-view-projection matrices per instance.
-                .with_buffer_builder(
-                    BufferBuilder::new::<InstanceData>(GeometryBufferKind::DynamicDraw, None)
-                        .with_attribute(AttributeDefinition {
-                            location: 7,
-                            kind: AttributeKind::UnsignedByte4,
-                            normalized: true,
-                            divisor: 1,
-                        })
-                        // World Matrix
-                        .with_attribute(AttributeDefinition {
-                            location: 8,
-                            kind: AttributeKind::Float4,
-                            normalized: false,
-                            divisor: 1,
-                        })
-                        .with_attribute(AttributeDefinition {
-                            location: 9,
-                            kind: AttributeKind::Float4,
-                            normalized: false,
-                            divisor: 1,
-                        })
-                        .with_attribute(AttributeDefinition {
-                            location: 10,
-                            kind: AttributeKind::Float4,
-                            normalized: false,
-                            divisor: 1,
-                        })
-                        .with_attribute(AttributeDefinition {
-                            location: 11,
-                            kind: AttributeKind::Float4,
-                            normalized: false,
-                            divisor: 1,
-                        })
-                        // Depth offset.
-                        .with_attribute(AttributeDefinition {
-                            location: 12,
-                            kind: AttributeKind::Float,
-                            normalized: false,
-                            divisor: 1,
-                        }),
-                )
-                .build(state)
-                .unwrap();
-
-            geometry_buffer.bind(state).set_triangles(data.triangles());
-
-            TimedEntry {
-                value: geometry_buffer,
-                time_to_live: 20.0,
-            }
-        });
-
-        geometry_buffer.time_to_live = 20.0;
-        geometry_buffer
-    }
-
-    fn update(&mut self, dt: f32) {
-        scope_profile!();
-
-        for entry in self.map.values_mut() {
-            entry.time_to_live -= dt;
-        }
-        self.map.retain(|_, v| v.time_to_live > 0.0);
-    }
-
-    fn clear(&mut self) {
-        self.map.clear();
-    }
-}
-
-#[derive(Default)]
-pub(in crate) struct TextureCache {
-    map: HashMap<usize, TimedEntry<Rc<RefCell<GpuTexture>>>>,
-}
-
-impl TextureCache {
-    fn get(
-        &mut self,
-        state: &mut PipelineState,
-        texture: &Texture,
-    ) -> Option<Rc<RefCell<GpuTexture>>> {
-        scope_profile!();
-
-        let key = texture.key();
-        let texture = texture.state();
-
-        if let TextureState::Ok(texture) = texture.deref() {
-            let entry = match self.map.entry(key) {
-                Entry::Occupied(e) => {
-                    let entry = e.into_mut();
-
-                    // Texture won't be destroyed while it used.
-                    entry.time_to_live = 20.0;
-
-                    // Check if some value has changed in resource.
-                    let mut tex = entry.borrow_mut();
-
-                    let new_mag_filter = texture.magnification_filter().into();
-                    if tex.magnification_filter() != new_mag_filter {
-                        tex.bind_mut(state, 0)
-                            .set_magnification_filter(new_mag_filter);
-                    }
-
-                    let new_min_filter = texture.minification_filter().into();
-                    if tex.minification_filter() != new_min_filter {
-                        tex.bind_mut(state, 0)
-                            .set_minification_filter(new_min_filter);
-                    }
-
-                    if tex.anisotropy().ne(&texture.anisotropy_level()) {
-                        tex.bind_mut(state, 0)
-                            .set_anisotropy(texture.anisotropy_level());
-                    }
-
-                    let new_s_wrap_mode = texture.s_wrap_mode().into();
-                    if tex.s_wrap_mode() != new_s_wrap_mode {
-                        tex.bind_mut(state, 0)
-                            .set_wrap(Coordinate::S, new_s_wrap_mode);
-                    }
-
-                    let new_t_wrap_mode = texture.t_wrap_mode().into();
-                    if tex.t_wrap_mode() != new_t_wrap_mode {
-                        tex.bind_mut(state, 0)
-                            .set_wrap(Coordinate::T, new_t_wrap_mode);
-                    }
-
-                    std::mem::drop(tex);
-
-                    entry
-                }
-                Entry::Vacant(e) => {
-                    let gpu_texture = match GpuTexture::new(
-                        state,
-                        texture.kind.into(),
-                        PixelKind::from(texture.pixel_kind),
-                        texture.minification_filter().into(),
-                        texture.magnification_filter().into(),
-                        texture.mip_count() as usize,
-                        Some(texture.bytes.as_slice()),
-                    ) {
-                        Ok(texture) => texture,
-                        Err(e) => {
-                            Log::writeln(
-                                MessageKind::Error,
-                                format!("Failed to create GPU texture. Reason: {:?}", e),
-                            );
-                            return None;
-                        }
-                    };
-
-                    e.insert(TimedEntry {
-                        value: Rc::new(RefCell::new(gpu_texture)),
-                        time_to_live: 20.0,
-                    })
-                }
-            };
-
-            Some(entry.value.clone())
-        } else {
-            None
-        }
-    }
-
-    fn update(&mut self, dt: f32) {
-        scope_profile!();
-
-        for entry in self.map.values_mut() {
-            entry.time_to_live -= dt;
-        }
-        self.map.retain(|_, v| v.time_to_live > 0.0);
-    }
-
-    fn clear(&mut self) {
-        self.map.clear();
-    }
-
-    fn unload(&mut self, texture: Texture) {
-        self.map.remove(&texture.key());
-    }
 }
 
 fn make_ui_frame_buffer(
