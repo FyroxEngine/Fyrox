df0c6fb3c9acb37ec49b0b1818515a95cd9a035b
Dmitry <d1maxa@yandex.ru>
2021-07-16 10:59:13 +0300

    continue with path fixer

--- src/utils/path_fixer.rs
+++ src/utils/path_fixer.rs
@@ -5,14 +5,17 @@ use crate::{
     gui::{BuildContext, Ui, UiMessage, UiNode},
     make_scene_file_filter,
 };
+use rg3d::gui::message::WidgetMessage;
 use rg3d::{
     core::{
+        color::Color,
         futures::executor::block_on,
         pool::Handle,
         visitor::{Visit, Visitor},
     },
     gui::{
         border::BorderBuilder,
+        brush::Brush,
         button::ButtonBuilder,
         decorator::DecoratorBuilder,
         file_browser::FileSelectorBuilder,
@@ -27,12 +30,16 @@ use rg3d::{
         text::TextBuilder,
         widget::WidgetBuilder,
         window::{WindowBuilder, WindowTitle},
-        HorizontalAlignment, Orientation, Thickness,
+        HorizontalAlignment, Orientation, Thickness, VerticalAlignment,
     },
     resource::{model::Model, texture::Texture},
     scene::{light::Light, node::Node, Scene},
 };
-use std::path::PathBuf;
+use std::{
+    collections::HashSet,
+    hash::{Hash, Hasher},
+    path::PathBuf,
+};
 
 pub struct PathFixer {
     pub window: Handle<UiNode>,
@@ -44,6 +51,9 @@ pub struct PathFixer {
     resources_list: Handle<UiNode>,
     cancel: Handle<UiNode>,
     ok: Handle<UiNode>,
+    selection: Option<usize>,
+    fix: Handle<UiNode>,
+    resource_path: Handle<UiNode>,
 }
 
 enum SceneResource {
@@ -59,7 +69,28 @@ impl SceneResource {
             SceneResource::Texture(texture) => texture.state().path().to_path_buf(),
         }
     }
+
+    fn key(&self) -> usize {
+        match self {
+            SceneResource::Model(model) => model.key(),
+            SceneResource::Texture(texture) => texture.key(),
+        }
+    }
 }
+
+impl Hash for SceneResource {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        state.write_usize(self.key());
+    }
+}
+
+impl PartialEq for SceneResource {
+    fn eq(&self, other: &Self) -> bool {
+        self.key() == other.key()
+    }
+}
+
+impl Eq for SceneResource {}
 
 impl PathFixer {
     pub fn new(ctx: &mut BuildContext) -> Self {
@@ -76,7 +107,9 @@ impl PathFixer {
         let resources_list;
         let cancel;
         let ok;
-        let window = WindowBuilder::new(WidgetBuilder::new().with_width(300.0).with_height(500.0))
+        let fix;
+        let resource_path;
+        let window = WindowBuilder::new(WidgetBuilder::new().with_width(400.0).with_height(500.0))
             .with_title(WindowTitle::text("Path Fixer"))
             .open(false)
             .with_content(
@@ -84,21 +117,52 @@ impl PathFixer {
                     WidgetBuilder::new()
                         .with_child({
                             scene_path = TextBuilder::new(WidgetBuilder::new().on_row(0))
-                                .with_text("No scene loaded!")
+                                .with_text("Scene: No scene loaded!")
                                 .with_wrap(WrapMode::Word)
                                 .build(ctx);
                             scene_path
                         })
+                        .with_child(
+                            GridBuilder::new(
+                                WidgetBuilder::new()
+                                    .on_row(1)
+                                    .with_child({
+                                        resource_path =
+                                            TextBuilder::new(WidgetBuilder::new().on_column(0))
+                                                .with_vertical_text_alignment(
+                                                    VerticalAlignment::Center,
+                                                )
+                                                .build(ctx);
+                                        resource_path
+                                    })
+                                    .with_child({
+                                        fix = ButtonBuilder::new(
+                                            WidgetBuilder::new()
+                                                .with_width(40.0)
+                                                .on_column(1)
+                                                .with_enabled(false)
+                                                .with_margin(Thickness::uniform(1.0)),
+                                        )
+                                        .with_text("Fix...")
+                                        .build(ctx);
+                                        fix
+                                    }),
+                            )
+                            .add_column(Column::stretch())
+                            .add_column(Column::auto())
+                            .add_row(Row::stretch())
+                            .build(ctx),
+                        )
                         .with_child({
                             resources_list =
-                                ListViewBuilder::new(WidgetBuilder::new().on_row(1)).build(ctx);
+                                ListViewBuilder::new(WidgetBuilder::new().on_row(2)).build(ctx);
                             resources_list
                         })
                         .with_child(
                             StackPanelBuilder::new(
                                 WidgetBuilder::new()
                                     .with_horizontal_alignment(HorizontalAlignment::Right)
-                                    .on_row(2)
+                                    .on_row(3)
                                     .with_child({
                                         load_scene = ButtonBuilder::new(
                                             WidgetBuilder::new()
@@ -135,6 +199,7 @@ impl PathFixer {
                         ),
                 )
                 .add_row(Row::auto())
+                .add_row(Row::strict(28.0))
                 .add_row(Row::stretch())
                 .add_row(Row::strict(28.0))
                 .add_column(Column::stretch())
@@ -152,6 +217,9 @@ impl PathFixer {
             resources_list,
             ok,
             cancel,
+            resource_path,
+            fix,
+            selection: None,
         }
     }
 
@@ -171,28 +239,31 @@ impl PathFixer {
                                 );
                             } else {
                                 // Gather resources.
-                                self.scene_resources.clear();
+
+                                // Use hash map to remove duplicates.
+                                let mut scene_resources = HashSet::new();
+
                                 for node in scene.graph.linear_iter() {
                                     if let Some(model) = node.resource() {
-                                        self.scene_resources.push(SceneResource::Model(model));
+                                        scene_resources.insert(SceneResource::Model(model));
                                     }
 
                                     match node {
                                         Node::Light(light) => {
                                             if let Light::Spot(spot) = light {
                                                 if let Some(texture) = spot.cookie_texture() {
-                                                    self.scene_resources.push(
-                                                        SceneResource::Texture(texture.clone()),
-                                                    );
+                                                    scene_resources.insert(SceneResource::Texture(
+                                                        texture.clone(),
+                                                    ));
                                                 }
                                             }
                                         }
                                         Node::Camera(camera) => {
                                             if let Some(skybox) = camera.skybox_ref() {
                                                 for texture in skybox.textures().iter().flatten() {
-                                                    self.scene_resources.push(
-                                                        SceneResource::Texture(texture.clone()),
-                                                    );
+                                                    scene_resources.insert(SceneResource::Texture(
+                                                        texture.clone(),
+                                                    ));
                                                 }
                                             }
                                         }
@@ -208,22 +279,22 @@ impl PathFixer {
                                                 .iter()
                                                 .flatten()
                                                 {
-                                                    self.scene_resources.push(
-                                                        SceneResource::Texture(texture.clone()),
-                                                    );
+                                                    scene_resources.insert(SceneResource::Texture(
+                                                        texture.clone(),
+                                                    ));
                                                 }
                                             }
                                         }
                                         Node::Sprite(sprite) => {
                                             if let Some(texture) = sprite.texture() {
-                                                self.scene_resources
-                                                    .push(SceneResource::Texture(texture));
+                                                scene_resources
+                                                    .insert(SceneResource::Texture(texture));
                                             }
                                         }
                                         Node::ParticleSystem(particle_system) => {
                                             if let Some(texture) = particle_system.texture() {
-                                                self.scene_resources
-                                                    .push(SceneResource::Texture(texture));
+                                                scene_resources
+                                                    .insert(SceneResource::Texture(texture));
                                             }
                                         }
                                         Node::Terrain(terrain) => {
@@ -239,7 +310,7 @@ impl PathFixer {
                                                     .iter()
                                                     .flatten()
                                                     {
-                                                        self.scene_resources.push(
+                                                        scene_resources.insert(
                                                             SceneResource::Texture(texture.clone()),
                                                         );
                                                     }
@@ -252,21 +323,45 @@ impl PathFixer {
                                     }
                                 }
 
+                                // Turn hash map into vec to be able to index it.
+                                self.scene_resources =
+                                    scene_resources.into_iter().collect::<Vec<_>>();
+
                                 let ctx = &mut ui.build_ctx();
                                 let items = self
                                     .scene_resources
                                     .iter()
-                                    .map(|r| {
-                                        DecoratorBuilder::new(BorderBuilder::new(
-                                            WidgetBuilder::new().with_child(
-                                                TextBuilder::new(WidgetBuilder::new())
-                                                    .with_text(
-                                                        r.path().to_string_lossy().to_string(),
-                                                    )
-                                                    .build(ctx),
-                                            ),
-                                        ))
-                                        .build(ctx)
+                                    .filter_map(|r| {
+                                        let path = r.path();
+                                        if !path.exists() {
+                                            Some(
+                                                DecoratorBuilder::new(BorderBuilder::new(
+                                                    WidgetBuilder::new()
+                                                        .with_height(22.0)
+                                                        .with_child(
+                                                            TextBuilder::new(
+                                                                WidgetBuilder::new()
+                                                                    .with_margin(
+                                                                        Thickness::uniform(1.0),
+                                                                    )
+                                                                    .with_foreground(Brush::Solid(
+                                                                        Color::RED,
+                                                                    )),
+                                                            )
+                                                            .with_vertical_text_alignment(
+                                                                VerticalAlignment::Center,
+                                                            )
+                                                            .with_text(
+                                                                path.to_string_lossy().to_string(),
+                                                            )
+                                                            .build(ctx),
+                                                        ),
+                                                ))
+                                                .build(ctx),
+                                            )
+                                        } else {
+                                            None
+                                        }
                                     })
                                     .collect::<Vec<_>>();
 
@@ -275,10 +370,15 @@ impl PathFixer {
                                     MessageDirection::ToWidget,
                                     items,
                                 ));
+                                ui.send_message(ListViewMessage::selection(
+                                    self.resources_list,
+                                    MessageDirection::ToWidget,
+                                    None,
+                                ));
 
                                 self.scene = Some(scene);
 
-                                message = path.to_string_lossy().to_string();
+                                message = format!("Scene: {}", path.display());
                             }
                         }
                         Err(e) => {
@@ -313,6 +413,34 @@ impl PathFixer {
                     ));
 
                     // TODO: Apply changes.
+                }
+            }
+            UiMessageData::ListView(ListViewMessage::SelectionChanged(selection)) => {
+                if message.destination() == self.resources_list {
+                    self.selection = selection.clone();
+
+                    if let Some(selection) = selection {
+                        ui.send_message(TextMessage::text(
+                            self.resource_path,
+                            MessageDirection::ToWidget,
+                            format!(
+                                "Resource: {}",
+                                self.scene_resources[*selection].path().display()
+                            ),
+                        ))
+                    } else {
+                        ui.send_message(TextMessage::text(
+                            self.resource_path,
+                            MessageDirection::ToWidget,
+                            "No resource selected".to_owned(),
+                        ));
+                    }
+
+                    ui.send_message(WidgetMessage::enabled(
+                        self.fix,
+                        MessageDirection::ToWidget,
+                        selection.is_some(),
+                    ));
                 }
             }
             _ => {}
