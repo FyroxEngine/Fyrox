f496355fda6363e0c13fea3abaf00008dda1bc67
Dmitry <d1maxa@yandex.ru>
2021-07-20 21:03:34 +0300

    added important comment for `feature` field in Intersection struct

--- src/scene/physics/mod.rs
+++ src/scene/physics/mod.rs
@@ -68,6 +68,13 @@ pub struct Intersection {
 
     /// Additional data that contains a kind of the feature with which
     /// intersection was detected as well as its index.    
+    ///
+    /// # Important notes.
+    ///
+    /// FeatureId::Face might have index that is greater than amount of triangles in
+    /// a triangle mesh, this means that intersection was detected from "back" side of
+    /// a face. To "fix" that index, simply subtract amount of triangles of a triangle
+    /// mesh from the value.
     pub feature: FeatureId,
 
     /// Distance from the ray origin.
