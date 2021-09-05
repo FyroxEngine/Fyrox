acd690a88741d6070e8ea9137b595e10a75205f6
Dmitry <d1maxa@yandex.ru>
2021-09-06 00:32:52 +0300

    improved Node docs
    - #172

--- src/scene/node.rs
+++ src/scene/node.rs
@@ -1,21 +1,21 @@
 //! Contains all structures and methods to create and manage scene graph nodes.
 //!
-//! Node is enumeration of possible types of scene nodes.
+//! For more info see [`Node`]
 
-use crate::scene::decal::Decal;
-use crate::scene::terrain::Terrain;
 use crate::{
-    core::define_is_as,
-    core::visitor::{Visit, VisitResult, Visitor},
+    core::{
+        define_is_as,
+        visitor::{Visit, VisitResult, Visitor},
+    },
     scene::{
-        base::Base, camera::Camera, light::Light, mesh::Mesh, particle_system::ParticleSystem,
-        sprite::Sprite,
+        base::Base, camera::Camera, decal::Decal, light::Light, mesh::Mesh,
+        particle_system::ParticleSystem, sprite::Sprite, terrain::Terrain,
     },
 };
 use std::ops::{Deref, DerefMut};
 
-/// Helper macros to reduce code bloat - its purpose it to dispatch
-/// specified call by actual enum variant.
+/// Helper macros to reduce code bloat - its purpose it to dispatch specified call by
+/// actual enum variant.
 macro_rules! static_dispatch {
     ($self:ident, $func:ident, $($args:expr),*) => {
         match $self {
@@ -43,24 +43,101 @@ impl Visit for Node {
     }
 }
 
-/// See module docs.
+/// Node is the basic building block for 3D scenes. It has multiple variants, but all of them share some
+/// common functionality:
+///
+/// - Local and global [transform](super::transform::Transform)
+/// - Info about connections with other nodes in scene
+/// - Visibility state - local and global
+/// - Name and tags
+/// - Level of details
+/// - Physics binding mode
+///
+/// The exact functionality depends on variant of the node, check the respective docs for a variant you
+/// interested in.
+///
+/// # Hierarchy
+///
+/// Nodes can be connected with other nodes, so a child node will be moved/rotate/scaled together with parent
+/// node. This has some analogy in real world - imagine a pen with a cap. The pen will be the parent node in
+/// the hierarchy and the cap will be child node. When you moving the pen, the cap moves with it only if it
+/// attached to the pen. The same principle works with scene nodes.
+///
+/// # Transform
+///
+/// The node has two kinds of transform - local and global. Local transform defines where the node is located
+/// (translation) relative to origin, how much it is scaled (in percent) and rotated (around any arbitrary axis).
+/// Global transform is almost the same, but it also includes the whole chain of transforms of parent nodes.
+/// In the previous example with the pen, the cap has its own local transform which tells how much it should be
+/// moved from origin to be exactly on top of the pen. But global transform of the cap includes transform of the
+/// pen. So if you move the pen, the local transform of the cap will remain the same, but global transform will
+/// include the transform of the pen.
+///
+/// # Name and tag
+///
+/// The node can have arbitrary name and tag. Both could be used to search the node in the graph. Unlike the name,
+/// tag could be used to store some gameplay information about the node. For example you can place a [`Mesh`] node
+/// that represents health pack model and it will have a name "HealthPack", in the tag you could put additional info
+/// like "MediumPack", "SmallPack", etc. So 3D model will not have "garbage" in its name, it will be stored inside tag.
+///
+/// # Visibility
+///
+/// The now has two kinds of visibility - local and global. As with transform, everything here is pretty similar.
+/// Local visibility defines if the node is visible as if it would be rendered alone, global visibility includes
+/// the combined visibility of entire chain of parent nodes.
+///
+/// Please keep in mind that "visibility" here means some sort of a "switch" that tells the renderer whether to draw
+/// the node or not. To fetch actual visibility of the node from a camera's perspective, use
+/// [visibility cache](super::camera::VisibilityCache) of the camera.
+///
+/// # Level of details
+///
+/// The node could control which children nodes should be drawn based on the distance to a camera, this is so called
+/// level of detail functionality. There is a separate article about LODs, it can be found [here](super::base::LevelOfDetail).
 #[derive(Debug)]
 pub enum Node {
-    /// See Base node docs.
+    /// A node that offers basic functionality, every other node shares this functionality.
+    ///
+    /// For more info see [`Base`] node docs.
     Base(Base),
-    /// See Light node docs.
+
+    /// A node that represents various light sources.
+    ///
+    /// For more info see [`Light`] node docs.
     Light(Light),
-    /// See Camera node docs.
+
+    /// A node that could be described as "our eyes in the world", a scene should have at least one camera for you
+    /// to be able to see anything.
+    ///
+    /// For more info see [`Camera`] node docs.
     Camera(Camera),
-    /// See Mesh node docs.
+
+    /// A node that is used for any kind of 3D models.
+    ///
+    /// For more info see [`Mesh`] node docs.
     Mesh(Mesh),
-    /// See Sprite node docs.
+
+    /// Special variation of [`Mesh`](Node::Mesh) variant which ensures that a rectangular face (billboard) is always rotated
+    /// in way so it always faces the camera.
+    ///
+    /// For more info see [`Sprite`] node docs.
     Sprite(Sprite),
-    /// See ParticleSystem node docs.
+
+    /// Collections of particles that is used to simulate clouds of particles, usually it is used to simulate dust, smoke, sparks
+    /// etc in scenes.
+    ///
+    /// For more info see [`ParticleSystem`] node docs.
     ParticleSystem(ParticleSystem),
-    /// See Terrain node docs.
+
+    /// A heightmap with multiple layers.
+    ///
+    /// For more info see [`Terrain`] node docs.
     Terrain(Terrain),
-    /// See Decal node docs.
+
+    /// A node that paints on other nodes using a texture. It is used to simulate cracks in concrete walls, damaged parts of the road,
+    /// blood splatters, bullet holes, etc.
+    ///
+    /// For more info see Decal node docs.
     Decal(Decal),
 }
 
