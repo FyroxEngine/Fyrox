354556ce36ecbd09e9e3cb66fa1cc2d2240069bf
Dmitry <d1maxa@yandex.ru>
2021-09-06 12:14:37 +0300

    improve Decal docs
    - #172

--- src/scene/decal.rs
+++ src/scene/decal.rs
@@ -1,18 +1,6 @@
-//! Decal is an image that gets projected to a geometry of a scene. Blood splatters, bullet holes, scratches
-//! etc. are done via decals.
+//! Decal is an image that gets projected to a geometry of a scene.
 //!
-//! # Size and transformations
-//!
-//! A decal defines a cube that projects a texture on every pixel of a scene that got into the cube. Exact cube
-//! size is defines by decal's `local scale`. For example, if you have a decal with scale (1.0, 2.0, 0.1) then
-//! the size of the cube (in local coordinates) will be `width = 1.0`, `height = 2.0`, `depth = 0.1`. The decal
-//! can be rotated as any other scene node. Its final size and orientation is defined by the chain of
-//! transformations of parent nodes.
-//!
-//! # Performance
-//!
-//! It should be noted that decals are not cheap, keep amount (and size) of decals at reasonable values! This
-//! means that unused decals (bullet holes for example) must be removed after some time.
+//! For more info see [`Decal`]
 
 use crate::{
     core::{color::Color, pool::Handle, visitor::prelude::*},
@@ -25,7 +13,66 @@ use crate::{
 };
 use std::ops::{Deref, DerefMut};
 
-/// See module docs.
+/// Decal is an image that gets projected to a geometry of a scene. Blood splatters, bullet holes, scratches
+/// etc. are done via decals.
+///
+/// # Size and transformations
+///
+/// A decal defines a cube that projects a texture on every pixel of a scene that got into the cube. Exact cube
+/// size is defines by decal's `local scale`. For example, if you have a decal with scale (1.0, 2.0, 0.1) then
+/// the size of the cube (in local coordinates) will be `width = 1.0`, `height = 2.0`, `depth = 0.1`. The decal
+/// can be rotated as any other scene node. Its final size and orientation is defined by the chain of
+/// transformations of parent nodes.
+///
+/// # Masking
+///
+/// Often you need to ensure that decal will be applied only on desired surfaces. For example a crack on the wall
+/// should not affect any surrounding objects, this can be achieved by using decal mask. Each decal has layer index,
+/// it will be drawn only if the index matches the index of the object that inside of decal bounds.
+///
+/// # Supported maps
+///
+/// Currently, only diffuse and normal maps are supported. Diffuse and normal maps will be automatically projected
+/// on the data stored in G-Buffer.
+///
+/// # Limitations
+///
+/// Current implementation works only with Deferred render path. Custom materials that uses Forward pass should
+/// implement decals manually.
+///
+/// # Performance
+///
+/// It should be noted that decals are not cheap, keep amount (and size) of decals at reasonable values! This
+/// means that unused decals (bullet holes for example) must be removed after some time.
+///
+/// # Example
+///
+/// ```
+/// use rg3d::{
+///         engine::resource_manager::ResourceManager,
+///         core::pool::Handle,
+///         scene::{
+///         node::Node,
+///         graph::Graph,
+///         decal::DecalBuilder,
+///         base::BaseBuilder,
+///         transform::TransformBuilder
+///     },
+///     core::algebra::Vector3
+/// };
+///
+/// fn create_bullet_hole(resource_manager: ResourceManager, graph: &mut Graph) -> Handle<Node> {
+///     DecalBuilder::new(
+///             BaseBuilder::new()
+///                 .with_local_transform(
+///                     TransformBuilder::new()
+///                         .with_local_scale(Vector3::new(2.0, 2.0, 2.0))
+///                         .build()
+///         ))
+///         .with_diffuse_texture(resource_manager.request_texture("bullet_hole.png", None))
+///         .build(graph)
+/// }
+/// ```
 #[derive(Debug, Visit, Default)]
 pub struct Decal {
     base: Base,
