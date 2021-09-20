187d7e494df19afc2635bfe6c85b82fdd967de30
toyboot4e <toyboot4e@gmail.com>
2021-09-20 18:57:07 +0900

    suppress macro trace

--- rg3d-core-derive/tests/it/main.rs
+++ rg3d-core-derive/tests/it/main.rs
@@ -2,9 +2,9 @@
 //!
 //! c.f. https://matklad.github.io/2021/02/27/delete-cargo-integration-tests.html
 
-#![feature(trace_macros)]
+// #![feature(trace_macros)]
 
 pub mod visit;
 
-trace_macros!(true);
+// trace_macros!(true);
 pub mod inspect;
