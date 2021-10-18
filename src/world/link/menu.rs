f716eab0fe20673972645bcb329ce02dd73c90bb
Dmitry Stepanov <d1maxa@yandex.ru>
2021-10-18 18:48:22 +0300

    `select target` for links

--- src/world/link/menu.rs
+++ src/world/link/menu.rs
@@ -15,25 +15,42 @@ pub struct LinkContextMenu {
     pub unlink: Handle<UiNode>,
     /// A link node above which the menu was opened.
     pub target: Handle<UiNode>,
+    pub select_target: Handle<UiNode>,
 }
 
 impl LinkContextMenu {
     pub fn new(ctx: &mut BuildContext) -> Self {
         let unlink;
+        let select_target;
         let menu = PopupBuilder::new(WidgetBuilder::new().with_visibility(false))
             .with_content(
-                StackPanelBuilder::new(WidgetBuilder::new().with_child({
-                    unlink = MenuItemBuilder::new(
-                        WidgetBuilder::new().with_min_size(Vector2::new(120.0, 20.0)),
-                    )
-                    .with_content(MenuItemContent::Text {
-                        text: "Unlink",
-                        shortcut: "",
-                        icon: Default::default(),
-                    })
-                    .build(ctx);
-                    unlink
-                }))
+                StackPanelBuilder::new(
+                    WidgetBuilder::new()
+                        .with_child({
+                            unlink = MenuItemBuilder::new(
+                                WidgetBuilder::new().with_min_size(Vector2::new(120.0, 20.0)),
+                            )
+                            .with_content(MenuItemContent::Text {
+                                text: "Unlink",
+                                shortcut: "",
+                                icon: Default::default(),
+                            })
+                            .build(ctx);
+                            unlink
+                        })
+                        .with_child({
+                            select_target = MenuItemBuilder::new(
+                                WidgetBuilder::new().with_min_size(Vector2::new(120.0, 20.0)),
+                            )
+                            .with_content(MenuItemContent::Text {
+                                text: "Select Target",
+                                shortcut: "",
+                                icon: Default::default(),
+                            })
+                            .build(ctx);
+                            select_target
+                        }),
+                )
                 .build(ctx),
             )
             .build(ctx);
@@ -41,6 +58,7 @@ impl LinkContextMenu {
         Self {
             menu,
             unlink,
+            select_target,
             target: Default::default(),
         }
     }
