24c5ea803b31159f13336ac81d527f501386f8c6
Dmitry Stepanov <d1maxa@yandex.ru>
2021-11-21 13:19:46 +0300

    added grid example

---
+++ examples/grid.rs
@@ -0,0 +1,54 @@
+use rg3d::{
+    engine::{framework::prelude::*, Engine},
+    gui::{
+        button::ButtonBuilder,
+        grid::{Column, GridBuilder, Row},
+        text::TextBuilder,
+        widget::WidgetBuilder,
+    },
+};
+
+struct Game {}
+
+impl GameState for Game {
+    fn init(engine: &mut Engine) -> Self
+    where
+        Self: Sized,
+    {
+        let ctx = &mut engine.user_interface.build_ctx();
+
+        GridBuilder::new(
+            WidgetBuilder::new()
+                .with_child(
+                    TextBuilder::new(WidgetBuilder::new().on_row(0).on_column(0))
+                        .with_text("Some")
+                        .build(ctx),
+                )
+                .with_child(
+                    TextBuilder::new(WidgetBuilder::new().on_row(0).on_column(1))
+                        .with_text("Text")
+                        .build(ctx),
+                )
+                .with_child(
+                    ButtonBuilder::new(WidgetBuilder::new().on_row(0).on_column(2))
+                        .with_text("TEST BUTTON")
+                        .build(ctx),
+                ),
+        )
+        .add_row(Row::stretch())
+        .add_column(Column::auto())
+        .add_column(Column::stretch())
+        .add_column(Column::auto())
+        .draw_border(true)
+        .build(ctx);
+
+        Self {}
+    }
+}
+
+fn main() {
+    Framework::<Game>::new()
+        .unwrap()
+        .title("Example - Grid")
+        .run();
+}
