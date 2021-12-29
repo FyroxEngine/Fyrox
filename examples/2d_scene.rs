41201f79b1ab06084219063d340d2f3a403b9129
Dmitry Stepanov <d1maxa@yandex.ru>
2021-12-29 20:10:58 +0300

    2d scene example improvements

--- examples/2d_scene.rs
+++ examples/2d_scene.rs
@@ -4,10 +4,12 @@
 //!
 //! This example shows simple 2D scene with light sources.
 
-use rg3d::scene::camera::{OrthographicProjection, Projection};
 use rg3d::{
-    core::{algebra::Vector3, pool::Handle},
-    engine::{framework::prelude::*, resource_manager::ResourceManager, Engine},
+    core::{algebra::Vector3, color::Color, futures::executor::block_on, pool::Handle},
+    engine::{
+        framework::prelude::*, resource_manager::MaterialSearchOptions,
+        resource_manager::ResourceManager, Engine,
+    },
     event::{ElementState, VirtualKeyCode, WindowEvent},
     event_loop::ControlFlow,
     gui::{
@@ -18,21 +20,17 @@ use rg3d::{
     },
     scene::{
         base::BaseBuilder,
-        camera::CameraBuilder,
+        camera::{CameraBuilder, OrthographicProjection, Projection},
         dim2::rectangle::RectangleBuilder,
-        light::{point::PointLightBuilder, spot::SpotLightBuilder, BaseLightBuilder},
         node::Node,
         transform::TransformBuilder,
         Scene,
     },
 };
-use rg3d_core::algebra::UnitQuaternion;
-use rg3d_core::color::Color;
 
 struct SceneLoader {
     scene: Scene,
     camera: Handle<Node>,
-    spot_light: Handle<Node>,
 }
 
 impl SceneLoader {
@@ -46,64 +44,19 @@ impl SceneLoader {
             .with_projection(Projection::Orthographic(OrthographicProjection {
                 z_near: -0.1,
                 z_far: 16.0,
-                vertical_size: 2.0,
+                vertical_size: 4.0,
             }))
             .build(&mut scene.graph);
 
-        // Add some sprites.
-        for y in 0..10 {
-            for x in 0..10 {
-                let sprite_size = 0.35;
-                let spacing = 0.1;
-                RectangleBuilder::new(
-                    BaseBuilder::new().with_local_transform(
-                        TransformBuilder::new()
-                            .with_local_position(Vector3::new(
-                                0.1 + x as f32 * (sprite_size + spacing),
-                                0.1 + y as f32 * (sprite_size + spacing),
-                                0.0, // Keep Z at zero.
-                            ))
-                            .with_local_scale(Vector3::new(sprite_size, sprite_size, f32::EPSILON))
-                            .build(),
-                    ),
-                )
-                .with_texture(resource_manager.request_texture("examples/data/starship.png", None))
-                .build(&mut scene.graph);
-            }
-        }
+        // Load scene.
+        block_on(resource_manager.request_model(
+            "examples/data/2d/scene.rgs",
+            MaterialSearchOptions::UsePathDirectly,
+        ))
+        .unwrap()
+        .instantiate_geometry(&mut scene);
 
-        // Add some lights.
-        PointLightBuilder::new(
-            BaseLightBuilder::new(
-                BaseBuilder::new().with_local_transform(
-                    TransformBuilder::new()
-                        .with_local_position(Vector3::new(2.5, 2.5, 0.0))
-                        .build(),
-                ),
-            )
-            .with_scatter_enabled(false),
-        )
-        .with_radius(1.0)
-        .build(&mut scene.graph);
-
-        let spot_light = SpotLightBuilder::new(
-            BaseLightBuilder::new(
-                BaseBuilder::new().with_local_transform(
-                    TransformBuilder::new()
-                        .with_local_position(Vector3::new(3.0, 1.0, 0.0))
-                        .build(),
-                ),
-            )
-            .with_scatter_enabled(false),
-        )
-        .with_distance(2.0)
-        .build(&mut scene.graph);
-
-        Self {
-            scene,
-            camera,
-            spot_light,
-        }
+        Self { scene, camera }
     }
 }
 
@@ -118,7 +71,6 @@ struct Game {
     input_controller: InputController,
     scene: Handle<Scene>,
     camera: Handle<Node>,
-    spot_light: Handle<Node>,
     debug_text: Handle<UiNode>,
 }
 
@@ -143,7 +95,6 @@ impl GameState for Game {
             // actions you need.
             scene: engine.scenes.add(loader.scene),
             camera: loader.camera,
-            spot_light: loader.spot_light,
             // Create simple user interface that will show some useful info.
             debug_text: TextBuilder::new(WidgetBuilder::new())
                 .build(&mut engine.user_interface.build_ctx()),
@@ -172,11 +123,6 @@ impl GameState for Game {
                 .local_transform_mut()
                 .offset(offset.scale(0.1));
         }
-
-        let local_transform = graph[self.spot_light].local_transform_mut();
-        let new_rotation = **local_transform.rotation()
-            * UnitQuaternion::from_euler_angles(0.0, 0.0, 1.0f32.to_radians());
-        local_transform.set_rotation(new_rotation);
 
         engine.user_interface.send_message(TextMessage::text(
             self.debug_text,
