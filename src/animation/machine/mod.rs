40fd1916901ea3945e442420f9b2f965c10a346e
Dmitry Stepanov <d1maxa@yandex.ru>
2022-04-21 21:05:01 +0300

    don't panic if there's invalid handle to a pose node

--- src/animation/machine/mod.rs
+++ src/animation/machine/mod.rs
@@ -306,9 +306,7 @@ impl MachineDefinition {
                             blend_pose.pose_source = node_map
                                 .get(&blend_pose_definition.pose_source)
                                 .cloned()
-                                .expect(
-                                    "There must be a respective pose node for blend pose source!",
-                                );
+                                .unwrap_or_default();
                         }
                     } else {
                         unreachable!()
@@ -326,7 +324,7 @@ impl MachineDefinition {
                             input.pose_source = node_map
                                 .get(&input_definition.pose_source)
                                 .cloned()
-                                .expect("There must be a respective pose node for indexed input!");
+                                .unwrap_or_default();
                         }
                     } else {
                         unreachable!()
