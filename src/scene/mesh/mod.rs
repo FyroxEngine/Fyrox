21b489b131067c9abd7a390c1c0feb3c8dc86e4c
Dmitry <d1maxa@yandex.ru>
2022-08-20 20:57:02 +0300

    build fix

--- src/scene/mesh/mod.rs
+++ src/scene/mesh/mod.rs
@@ -131,10 +131,7 @@ impl Default for Mesh {
     fn default() -> Self {
         Self {
             base: Default::default(),
-            surfaces: TemplateVariable::new_with_flags(
-                Default::default(),
-                VariableFlags::DONT_MARK_AS_MODIFIED_IF_MISSING,
-            ),
+            surfaces: Default::default(),
             local_bounding_box: Default::default(),
             world_bounding_box: Default::default(),
             local_bounding_box_dirty: Cell::new(true),
