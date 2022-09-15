bc355c8ac3dc79cfd88b715d07b46138fc1537ec
Dmitry <d1maxa@yandex.ru>
2022-09-15 22:03:08 +0300

    `#[inline]` for `Base`
    - cross-crate inlining is off by default, this change ensures that the methods will be inlined

--- src/scene/base.rs
+++ src/scene/base.rs
@@ -2,7 +2,6 @@
 //!
 //! For more info see [`Base`]
 
-use crate::script::ScriptTrait;
 use crate::{
     core::{
         algebra::{Matrix4, Vector3},
@@ -17,7 +16,7 @@ use crate::{
     engine::{resource_manager::ResourceManager, SerializationContext},
     resource::model::Model,
     scene::{graph::map::NodeHandleMap, node::Node, transform::Transform},
-    script::Script,
+    script::{Script, ScriptTrait},
     utils::log::Log,
 };
 use std::{
@@ -439,6 +438,7 @@ impl Clone for Base {
 
 impl Base {
     /// Sets name of node. Can be useful to mark a node to be able to find it later on.
+    #[inline]
     pub fn set_name<N: AsRef<str>>(&mut self, name: N) {
         self.set_name_internal(name.as_ref().to_owned());
     }
@@ -448,45 +448,53 @@ impl Base {
     }
 
     /// Returns name of node.
+    #[inline]
     pub fn name(&self) -> &str {
         self.name.as_str()
     }
 
     /// Returns owned name of node.
+    #[inline]
     pub fn name_owned(&self) -> String {
         (*self.name).clone()
     }
 
     /// Returns shared reference to local transform of a node, can be used to fetch
     /// some local spatial properties, such as position, rotation, scale, etc.
+    #[inline]
     pub fn local_transform(&self) -> &Transform {
         &self.local_transform
     }
 
     /// Returns mutable reference to local transform of a node, can be used to set
     /// some local spatial properties, such as position, rotation, scale, etc.
+    #[inline]
     pub fn local_transform_mut(&mut self) -> &mut Transform {
         self.transform_modified.set(true);
         &mut self.local_transform
     }
 
     /// Sets new local transform of a node.
+    #[inline]
     pub fn set_local_transform(&mut self, transform: Transform) {
         self.local_transform = transform;
     }
 
     /// Tries to find properties by the name. The method returns an iterator because it possible
     /// to have multiple properties with the same name.
+    #[inline]
     pub fn find_properties_ref<'a>(&'a self, name: &'a str) -> impl Iterator<Item = &'a Property> {
         self.properties.iter().filter(move |p| p.name == name)
     }
 
     /// Tries to find a first property with the given name.
+    #[inline]
     pub fn find_first_property_ref(&self, name: &str) -> Option<&Property> {
         self.properties.iter().find(|p| p.name == name)
     }
 
     /// Sets a new set of properties of the node.
+    #[inline]
     pub fn set_properties(&mut self, properties: Vec<Property>) -> Vec<Property> {
         std::mem::replace(self.properties.get_mut(), properties)
     }
@@ -500,6 +508,7 @@ impl Base {
     /// system node and it will be removed from scene when time will end. This is
     /// efficient algorithm because scene holds every object in pool and allocation
     /// or deallocation of node takes very little amount of time.
+    #[inline]
     pub fn set_lifetime(&mut self, time_seconds: Option<f32>) -> &mut Self {
         self.lifetime.set(time_seconds);
         self
@@ -507,17 +516,20 @@ impl Base {
 
     /// Returns current lifetime of a node. Will be None if node has undefined lifetime.
     /// For more info about lifetimes see [`set_lifetime`](Self::set_lifetime).
+    #[inline]
     pub fn lifetime(&self) -> Option<f32> {
         *self.lifetime
     }
 
     /// Returns handle of parent node.
+    #[inline]
     pub fn parent(&self) -> Handle<Node> {
         self.parent
     }
 
     /// Returns slice of handles to children nodes. This can be used, for example, to
     /// traverse tree starting from some node.
+    #[inline]
     pub fn children(&self) -> &[Handle<Node>] {
         self.children.as_slice()
     }
@@ -525,6 +537,7 @@ impl Base {
     /// Returns global transform matrix, such matrix contains combined transformation
     /// of transforms of parent nodes. This is the final matrix that describes real
     /// location of object in the world.
+    #[inline]
     pub fn global_transform(&self) -> Matrix4<f32> {
         self.global_transform.get()
     }
@@ -532,26 +545,31 @@ impl Base {
     /// Returns inverse of bind pose matrix. Bind pose matrix - is special matrix
     /// for bone nodes, it stores initial transform of bone node at the moment
     /// of "binding" vertices to bones.
+    #[inline]
     pub fn inv_bind_pose_transform(&self) -> Matrix4<f32> {
         self.inv_bind_pose_transform
     }
 
     /// Returns true if this node is model resource instance root node.
+    #[inline]
     pub fn is_resource_instance_root(&self) -> bool {
         self.is_resource_instance_root
     }
 
     /// Returns resource from which this node was instantiated from.
+    #[inline]
     pub fn resource(&self) -> Option<Model> {
         self.resource.clone()
     }
 
     /// Sets local visibility of a node.
+    #[inline]
     pub fn set_visibility(&mut self, visibility: bool) -> bool {
         self.visibility.set(visibility)
     }
 
     /// Returns local visibility of a node.
+    #[inline]
     pub fn visibility(&self) -> bool {
         *self.visibility
     }
@@ -564,6 +582,7 @@ impl Base {
     }
 
     /// Returns current **world-space** bounding box.
+    #[inline]
     pub fn world_bounding_box(&self) -> AxisAlignedBoundingBox {
         self.local_bounding_box()
             .transform(&self.global_transform())
@@ -573,11 +592,13 @@ impl Base {
     ///
     /// TODO. Mobility still has no effect, it was designed to be used in combined
     /// rendering (dynamic + static lights (lightmaps))
+    #[inline]
     pub fn set_mobility(&mut self, mobility: Mobility) -> Mobility {
         self.mobility.set(mobility)
     }
 
     /// Return current mobility of the node.
+    #[inline]
     pub fn mobility(&self) -> Mobility {
         *self.mobility
     }
@@ -587,6 +608,7 @@ impl Base {
     /// all its children will be invisible. It defines if object will be rendered. It is *not* the same as real
     /// visibility from point of view of a camera. To check if object is visible from some camera, use
     /// [VisibilityCache](super::visibility::VisibilityCache). However this still can't tell you if object is behind obstacle or not.
+    #[inline]
     pub fn global_visibility(&self) -> bool {
         self.global_visibility.get()
     }
@@ -597,26 +619,31 @@ impl Base {
     ///
     /// This handle is extensively used to fetch information about the state of node in the resource
     /// to sync properties of instance with its original in the resource.
+    #[inline]
     pub fn original_handle_in_resource(&self) -> Handle<Node> {
         self.original_handle_in_resource
     }
 
     /// Returns position of the node in absolute coordinates.
+    #[inline]
     pub fn global_position(&self) -> Vector3<f32> {
         self.global_transform.get().position()
     }
 
     /// Returns "look" vector of global transform basis, in most cases return vector will be non-normalized.
+    #[inline]
     pub fn look_vector(&self) -> Vector3<f32> {
         self.global_transform.get().look()
     }
 
     /// Returns "side" vector of global transform basis, in most cases return vector will be non-normalized.
+    #[inline]
     pub fn side_vector(&self) -> Vector3<f32> {
         self.global_transform.get().side()
     }
 
     /// Returns "up" vector of global transform basis, in most cases return vector will be non-normalized.
+    #[inline]
     pub fn up_vector(&self) -> Vector3<f32> {
         self.global_transform.get().up()
     }
@@ -629,56 +656,67 @@ impl Base {
     /// This value is used to modify projection matrix before render node. Element m\[4\]\[3\] of projection
     /// matrix usually set to -1 to which makes w coordinate of in homogeneous space to be -z_fragment for
     /// further perspective divide. We can abuse this to shift z of fragment by some value.
+    #[inline]
     pub fn set_depth_offset_factor(&mut self, factor: f32) -> f32 {
         self.depth_offset.set(factor.abs().min(1.0).max(0.0))
     }
 
     /// Returns depth offset factor.
+    #[inline]
     pub fn depth_offset_factor(&self) -> f32 {
         *self.depth_offset
     }
 
     /// Sets new lod group.
+    #[inline]
     pub fn set_lod_group(&mut self, lod_group: Option<LodGroup>) -> Option<LodGroup> {
         std::mem::replace(self.lod_group.get_mut(), lod_group)
     }
 
     /// Extracts lod group, leaving None in the node.
+    #[inline]
     pub fn take_lod_group(&mut self) -> Option<LodGroup> {
         std::mem::take(self.lod_group.get_mut())
     }
 
     /// Returns shared reference to current lod group.
+    #[inline]
     pub fn lod_group(&self) -> Option<&LodGroup> {
         self.lod_group.as_ref()
     }
 
     /// Returns mutable reference to current lod group.
+    #[inline]
     pub fn lod_group_mut(&mut self) -> Option<&mut LodGroup> {
         self.lod_group.get_mut().as_mut()
     }
 
     /// Returns node tag.
+    #[inline]
     pub fn tag(&self) -> &str {
         &self.tag
     }
 
     /// Returns a copy of node tag.
+    #[inline]
     pub fn tag_owned(&self) -> String {
         (*self.tag).clone()
     }
 
     /// Sets new tag.
+    #[inline]
     pub fn set_tag(&mut self, tag: String) -> String {
         self.tag.set(tag)
     }
 
     /// Return the frustum_culling flag
+    #[inline]
     pub fn frustum_culling(&self) -> bool {
         *self.frustum_culling
     }
 
     /// Sets whether to use frustum culling or not
+    #[inline]
     pub fn set_frustum_culling(&mut self, frustum_culling: bool) -> bool {
         self.frustum_culling.set(frustum_culling)
     }
@@ -943,6 +981,7 @@ impl Default for BaseBuilder {
 
 impl BaseBuilder {
     /// Creates new builder instance.
+    #[inline]
     pub fn new() -> Self {
         Self {
             name: Default::default(),
@@ -962,36 +1001,42 @@ impl BaseBuilder {
     }
 
     /// Sets desired mobility.
+    #[inline]
     pub fn with_mobility(mut self, mobility: Mobility) -> Self {
         self.mobility = mobility;
         self
     }
 
     /// Sets desired name.
+    #[inline]
     pub fn with_name<P: AsRef<str>>(mut self, name: P) -> Self {
         self.name = name.as_ref().to_owned();
         self
     }
 
     /// Sets desired visibility.
+    #[inline]
     pub fn with_visibility(mut self, visibility: bool) -> Self {
         self.visibility = visibility;
         self
     }
 
     /// Sets desired local transform.
+    #[inline]
     pub fn with_local_transform(mut self, transform: Transform) -> Self {
         self.local_transform = transform;
         self
     }
 
     /// Sets desired inverse bind pose transform.
+    #[inline]
     pub fn with_inv_bind_pose_transform(mut self, inv_bind_pose: Matrix4<f32>) -> Self {
         self.inv_bind_pose_transform = inv_bind_pose;
         self
     }
 
     /// Sets desired list of children nodes.
+    #[inline]
     pub fn with_children<'a, I: IntoIterator<Item = &'a Handle<Node>>>(
         mut self,
         children: I,
@@ -1005,48 +1050,56 @@ impl BaseBuilder {
     }
 
     /// Sets desired lifetime.
+    #[inline]
     pub fn with_lifetime(mut self, time_seconds: f32) -> Self {
         self.lifetime = Some(time_seconds);
         self
     }
 
     /// Sets desired depth offset.
+    #[inline]
     pub fn with_depth_offset(mut self, offset: f32) -> Self {
         self.depth_offset = offset;
         self
     }
 
     /// Sets desired lod group.
+    #[inline]
     pub fn with_lod_group(mut self, lod_group: LodGroup) -> Self {
         self.lod_group = Some(lod_group);
         self
     }
 
     /// Sets desired tag.
+    #[inline]
     pub fn with_tag(mut self, tag: String) -> Self {
         self.tag = tag;
         self
     }
 
     /// Sets desired frustum_culling flag.
+    #[inline]
     pub fn with_frustum_culling(mut self, frustum_culling: bool) -> Self {
         self.frustum_culling = frustum_culling;
         self
     }
 
     /// Sets whether mesh should cast shadows or not.
+    #[inline]
     pub fn with_cast_shadows(mut self, cast_shadows: bool) -> Self {
         self.cast_shadows = cast_shadows;
         self
     }
 
     /// Sets desired script of the node.
+    #[inline]
     pub fn with_script(mut self, script: Script) -> Self {
         self.script = Some(script);
         self
     }
 
     /// Creates an instance of [`Base`].
+    #[inline]
     pub fn build_base(self) -> Base {
         Base {
             self_handle: Default::default(),
