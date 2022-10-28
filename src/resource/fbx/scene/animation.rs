7dd23be871185d3eef362c95b1828b1b1be75182
Dmitry <d1maxa@yandex.ru>
2022-10-28 19:39:47 +0300

    removed unused methods

--- src/resource/fbx/scene/animation.rs
+++ src/resource/fbx/scene/animation.rs
@@ -1,13 +1,9 @@
-use crate::core::algebra::{UnitQuaternion, Vector3};
-use crate::utils::log::MessageKind;
 use crate::{
     core::pool::Handle,
     resource::fbx::{
         document::{FbxNode, FbxNodeContainer},
-        quat_from_euler,
-        scene::{FbxComponent, FbxScene, FBX_TIME_UNIT},
+        scene::{FbxComponent, FBX_TIME_UNIT},
     },
-    utils::log::Log,
 };
 use fxhash::FxHashMap;
 
@@ -48,44 +44,6 @@ impl FbxAnimationCurve {
 
         Ok(curve)
     }
-
-    fn eval(&self, time: f32) -> f32 {
-        if self.keys.is_empty() {
-            Log::writeln(
-                MessageKind::Warning,
-                "FBX: Trying to evaluate curve with no keys!",
-            );
-
-            return 0.0;
-        }
-
-        if time <= self.keys[0].time {
-            return self.keys[0].value;
-        }
-
-        if time >= self.keys[self.keys.len() - 1].time {
-            return self.keys[self.keys.len() - 1].value;
-        }
-
-        // Do linear search for span
-        for i in 0..(self.keys.len() - 1) {
-            let cur = &self.keys[i];
-            if cur.time >= time {
-                let next = &self.keys[i + 1];
-
-                // calculate interpolation coefficient
-                let time_span = next.time - cur.time;
-                let k = (time - cur.time) / time_span;
-
-                // TODO: for now assume that we have only linear transitions
-                let val_span = next.value - cur.value;
-                return cur.value + k * val_span;
-            }
-        }
-
-        // Edge-case when we are at the end of curve.
-        self.keys.last().unwrap().value
-    }
 }
 
 #[derive(PartialEq, Eq)]
@@ -115,36 +73,5 @@ impl FbxAnimationCurveNode {
             },
             curves: Default::default(),
         })
-    }
-
-    pub fn eval_vec3(&self, scene: &FbxScene, default: Vector3<f32>, time: f32) -> Vector3<f32> {
-        if self.curves.is_empty() {
-            default
-        } else {
-            let fetch_curve = |name: &str, default: f32| {
-                if let Some(FbxComponent::AnimationCurve(curve)) =
-                    self.curves.get(name).map(|c| scene.get(*c))
-                {
-                    curve.eval(time)
-                } else {
-                    default
-                }
-            };
-
-            Vector3::new(
-                fetch_curve("d|X", default[0]),
-                fetch_curve("d|Y", default[1]),
-                fetch_curve("d|Z", default[2]),
-            )
-        }
-    }
-
-    pub fn eval_quat(
-        &self,
-        scene: &FbxScene,
-        default_euler: Vector3<f32>,
-        time: f32,
-    ) -> UnitQuaternion<f32> {
-        quat_from_euler(self.eval_vec3(scene, default_euler, time))
     }
 }
