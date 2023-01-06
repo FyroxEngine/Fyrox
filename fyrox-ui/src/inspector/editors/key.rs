3ead8a51fd1efa5d4812b9ab10131c939fbafe8d
Dmitry <d1maxa@yandex.ru>
2023-01-06 22:05:59 +0300

    key binding + editor + property editor
    - part of #366

--- fyrox-ui/src/inspector/editors/key.rs
+++ fyrox-ui/src/inspector/editors/key.rs
@@ -6,7 +6,10 @@ use crate::{
         },
         FieldKind, InspectorError, PropertyChanged,
     },
-    key::{HotKey, HotKeyEditorBuilder, HotKeyEditorMessage},
+    key::{
+        HotKey, HotKeyEditorBuilder, HotKeyEditorMessage, KeyBinding, KeyBindingEditorBuilder,
+        KeyBindingEditorMessage,
+    },
     message::{MessageDirection, UiMessage},
     widget::WidgetBuilder,
     Thickness,
@@ -50,6 +53,54 @@ impl PropertyEditorDefinition for HotKeyPropertyEditorDefinition {
     fn translate_message(&self, ctx: PropertyEditorTranslationContext) -> Option<PropertyChanged> {
         if ctx.message.direction() == MessageDirection::FromWidget {
             if let Some(HotKeyEditorMessage::Value(value)) = ctx.message.data() {
+                return Some(PropertyChanged {
+                    name: ctx.name.to_string(),
+                    owner_type_id: ctx.owner_type_id,
+                    value: FieldKind::object(value.clone()),
+                });
+            }
+        }
+        None
+    }
+}
+
+#[derive(Debug)]
+pub struct KeyBindingPropertyEditorDefinition;
+
+impl PropertyEditorDefinition for KeyBindingPropertyEditorDefinition {
+    fn value_type_id(&self) -> TypeId {
+        TypeId::of::<KeyBinding>()
+    }
+
+    fn create_instance(
+        &self,
+        ctx: PropertyEditorBuildContext,
+    ) -> Result<PropertyEditorInstance, InspectorError> {
+        let value = ctx.property_info.cast_value::<KeyBinding>()?;
+        Ok(PropertyEditorInstance::Simple {
+            editor: KeyBindingEditorBuilder::new(
+                WidgetBuilder::new().with_margin(Thickness::uniform(1.0)),
+            )
+            .with_value(value.clone())
+            .build(ctx.build_context),
+        })
+    }
+
+    fn create_message(
+        &self,
+        ctx: PropertyEditorMessageContext,
+    ) -> Result<Option<UiMessage>, InspectorError> {
+        let value = ctx.property_info.cast_value::<KeyBinding>()?;
+        Ok(Some(KeyBindingEditorMessage::value(
+            ctx.instance,
+            MessageDirection::ToWidget,
+            value.clone(),
+        )))
+    }
+
+    fn translate_message(&self, ctx: PropertyEditorTranslationContext) -> Option<PropertyChanged> {
+        if ctx.message.direction() == MessageDirection::FromWidget {
+            if let Some(KeyBindingEditorMessage::Value(value)) = ctx.message.data() {
                 return Some(PropertyChanged {
                     name: ctx.name.to_string(),
                     owner_type_id: ctx.owner_type_id,
--- fyrox-ui/src/key.rs
+++ fyrox-ui/src/key.rs
@@ -31,6 +31,36 @@ impl HotKey {
             modifiers: Default::default(),
         }
     }
+
+    pub fn ctrl_key(key: KeyCode) -> Self {
+        Self::Some {
+            code: key,
+            modifiers: KeyboardModifiers {
+                control: true,
+                ..Default::default()
+            },
+        }
+    }
+
+    pub fn shift_key(key: KeyCode) -> Self {
+        Self::Some {
+            code: key,
+            modifiers: KeyboardModifiers {
+                shift: true,
+                ..Default::default()
+            },
+        }
+    }
+
+    pub fn alt_key(key: KeyCode) -> Self {
+        Self::Some {
+            code: key,
+            modifiers: KeyboardModifiers {
+                shift: true,
+                ..Default::default()
+            },
+        }
+    }
 }
 
 impl Default for HotKey {
@@ -206,6 +236,181 @@ impl HotKeyEditorBuilder {
             .build(ctx);
 
         let editor = HotKeyEditor {
+            widget: self.widget_builder.with_child(text).build(),
+            text,
+            editing: false,
+            value: self.value,
+        };
+
+        ctx.add_node(UiNode::new(editor))
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, Reflect)]
+pub enum KeyBinding {
+    NotSet,
+    Some(KeyCode),
+}
+
+impl PartialEq<KeyCode> for KeyBinding {
+    fn eq(&self, other: &KeyCode) -> bool {
+        match self {
+            KeyBinding::NotSet => false,
+            KeyBinding::Some(code) => code == other,
+        }
+    }
+}
+
+impl KeyBinding {
+    pub fn from_key_code(key: KeyCode) -> Self {
+        Self::Some(key)
+    }
+}
+
+impl Default for KeyBinding {
+    fn default() -> Self {
+        Self::NotSet
+    }
+}
+
+impl Display for KeyBinding {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::NotSet => f.write_str("Not Set"),
+            Self::Some(code) => write!(f, "{}", code.as_ref()),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum KeyBindingEditorMessage {
+    Value(KeyBinding),
+}
+
+impl KeyBindingEditorMessage {
+    define_constructor!(KeyBindingEditorMessage:Value => fn value(KeyBinding), layout: false);
+}
+
+#[derive(Clone)]
+pub struct KeyBindingEditor {
+    widget: Widget,
+    text: Handle<UiNode>,
+    value: KeyBinding,
+    editing: bool,
+}
+
+define_widget_deref!(KeyBindingEditor);
+
+impl KeyBindingEditor {
+    fn set_editing(&mut self, editing: bool, ui: &UserInterface) {
+        self.editing = editing;
+        ui.send_message(TextMessage::text(
+            self.text,
+            MessageDirection::ToWidget,
+            if self.editing {
+                "[WAITING INPUT]".to_string()
+            } else {
+                format!("{}", self.value)
+            },
+        ));
+    }
+}
+
+impl Control for KeyBindingEditor {
+    fn query_component(&self, type_id: TypeId) -> Option<&dyn Any> {
+        if type_id == TypeId::of::<Self>() {
+            Some(self)
+        } else {
+            None
+        }
+    }
+
+    fn draw(&self, drawing_context: &mut DrawingContext) {
+        // Make background clickable.
+        drawing_context.push_rect_filled(&self.bounding_rect(), None);
+        drawing_context.commit(
+            self.clip_bounds(),
+            Brush::Solid(Color::TRANSPARENT),
+            CommandTexture::None,
+            None,
+        );
+    }
+
+    fn handle_routed_message(&mut self, ui: &mut UserInterface, message: &mut UiMessage) {
+        self.widget.handle_routed_message(ui, message);
+
+        if let Some(msg) = message.data::<WidgetMessage>() {
+            match msg {
+                WidgetMessage::KeyDown(key) => {
+                    ui.send_message(KeyBindingEditorMessage::value(
+                        self.handle,
+                        MessageDirection::ToWidget,
+                        KeyBinding::Some(*key),
+                    ));
+
+                    message.set_handled(true);
+                }
+                WidgetMessage::MouseDown { button, .. } => {
+                    if *button == MouseButton::Left {
+                        if self.editing {
+                            self.set_editing(false, ui);
+                        } else {
+                            self.set_editing(true, ui);
+                        }
+                    }
+                }
+                WidgetMessage::Unfocus => {
+                    if self.editing {
+                        self.set_editing(false, ui);
+                    }
+                }
+                _ => (),
+            }
+        }
+
+        if message.destination() == self.handle && message.direction() == MessageDirection::ToWidget
+        {
+            if let Some(KeyBindingEditorMessage::Value(value)) = message.data() {
+                if value != &self.value {
+                    self.value = value.clone();
+
+                    ui.send_message(TextMessage::text(
+                        self.text,
+                        MessageDirection::ToWidget,
+                        format!("{}", self.value),
+                    ));
+
+                    ui.send_message(message.reverse());
+                }
+            }
+        }
+    }
+}
+
+pub struct KeyBindingEditorBuilder {
+    widget_builder: WidgetBuilder,
+    value: KeyBinding,
+}
+
+impl KeyBindingEditorBuilder {
+    pub fn new(widget_builder: WidgetBuilder) -> Self {
+        Self {
+            widget_builder,
+            value: KeyBinding::NotSet,
+        }
+    }
+
+    pub fn with_value(mut self, key_binding: KeyBinding) -> Self {
+        self.value = key_binding;
+        self
+    }
+
+    pub fn build(self, ctx: &mut BuildContext) -> Handle<UiNode> {
+        let text = TextBuilder::new(WidgetBuilder::new())
+            .with_text(format!("{}", self.value))
+            .build(ctx);
+
+        let editor = KeyBindingEditor {
             widget: self.widget_builder.with_child(text).build(),
             text,
             editing: false,
