690beed8fea83292a3aac09222c5d194fc4af251
Dmitry <d1maxa@yandex.ru>
2023-01-22 13:59:09 +0300

    fixed fyrox-core-derive tests (all passed)

--- fyrox-core-derive/tests/it/reflect.rs
+++ fyrox-core-derive/tests/it/reflect.rs
@@ -5,10 +5,11 @@
 use std::any::TypeId;
 use std::ops::{Deref, DerefMut};
 
+use fyrox_core::parking_lot::Mutex;
 use fyrox_core::reflect::*;
 
 #[allow(dead_code)]
-#[derive(Reflect, Debug)]
+#[derive(Reflect, Debug, Clone)]
 pub struct Struct {
     field: usize,
     #[reflect(hidden)]
@@ -46,37 +47,37 @@ fn reflect_field_accessors() {
         hidden: 10,
     };
 
-    assert_eq!(s.get_field::<usize>(Struct::FIELD), Some(&10));
-    assert_eq!(s.get_field_mut::<usize>(Struct::FIELD), Some(&mut 10));
-    *s.get_field_mut::<usize>(Struct::FIELD).unwrap() = 100;
-    assert_eq!(s.get_field::<usize>(Struct::FIELD), Some(&100));
+    s.get_field::<usize>(Struct::FIELD, &mut |field| assert_eq!(field, Some(&10)));
+    s.get_field_mut::<usize>(Struct::FIELD, &mut |field| assert_eq!(field, Some(&mut 10)));
+    s.get_field_mut::<usize>(Struct::FIELD, &mut |field| *field.unwrap() = 100);
+    s.get_field::<usize>(Struct::FIELD, &mut |field| assert_eq!(field, Some(&100)));
 
-    assert!(s.get_field::<usize>("HIDDEN").is_none());
+    s.get_field::<usize>("HIDDEN", &mut |field| assert!(field.is_none()));
 
     let mut t = Tuple(0, 1);
 
-    assert_eq!(t.get_field::<usize>(Tuple::F_0), Some(&0));
-    assert_eq!(t.get_field::<usize>(Tuple::F_1), Some(&1));
+    t.get_field::<usize>(Tuple::F_0, &mut |field| assert_eq!(field, Some(&0)));
+    t.get_field::<usize>(Tuple::F_1, &mut |field| assert_eq!(field, Some(&1)));
 
-    *t.get_field_mut::<usize>(Tuple::F_0).unwrap() += 10;
-    *t.get_field_mut::<usize>(Tuple::F_1).unwrap() += 10;
+    t.get_field_mut::<usize>(Tuple::F_0, &mut |field| *field.unwrap() += 10);
+    t.get_field_mut::<usize>(Tuple::F_1, &mut |field| *field.unwrap() += 10);
 
-    assert_eq!(t.get_field::<usize>(Tuple::F_0), Some(&10));
-    assert_eq!(t.get_field::<usize>(Tuple::F_1), Some(&11));
+    t.get_field::<usize>(Tuple::F_0, &mut |field| assert_eq!(field, Some(&10)));
+    t.get_field::<usize>(Tuple::F_1, &mut |field| assert_eq!(field, Some(&11)));
 
     let mut e_named = Enum::Named { field: 10 };
 
-    assert_eq!(e_named.get_field::<usize>(Enum::NAMED_FIELD), Some(&10));
-    assert_eq!(e_named.get_field::<usize>(Enum::TUPLE_F_0), None);
-    *e_named.get_field_mut::<usize>(Enum::NAMED_FIELD).unwrap() = 20usize;
-    assert_eq!(e_named.get_field::<usize>(Enum::NAMED_FIELD), Some(&20));
+    e_named.get_field::<usize>(Enum::NAMED_FIELD, &mut |field| assert_eq!(field, Some(&10)));
+    e_named.get_field::<usize>(Enum::TUPLE_F_0, &mut |field| assert_eq!(field, None));
+    e_named.get_field_mut::<usize>(Enum::NAMED_FIELD, &mut |field| *field.unwrap() = 20usize);
+    e_named.get_field::<usize>(Enum::NAMED_FIELD, &mut |field| assert_eq!(field, Some(&20)));
 
     let mut e_tuple = Enum::Tuple(30);
 
-    assert_eq!(e_tuple.get_field::<usize>(Enum::NAMED_FIELD), None);
-    assert_eq!(e_tuple.get_field::<usize>(Enum::TUPLE_F_0), Some(&30));
-    *e_tuple.get_field_mut::<usize>(Enum::TUPLE_F_0).unwrap() = 40usize;
-    assert_eq!(e_tuple.get_field::<usize>(Enum::TUPLE_F_0), Some(&40));
+    e_tuple.get_field::<usize>(Enum::NAMED_FIELD, &mut |field| assert_eq!(field, None));
+    e_tuple.get_field::<usize>(Enum::TUPLE_F_0, &mut |field| assert_eq!(field, Some(&30)));
+    e_tuple.get_field_mut::<usize>(Enum::TUPLE_F_0, &mut |field| *field.unwrap() = 40usize);
+    e_tuple.get_field::<usize>(Enum::TUPLE_F_0, &mut |field| assert_eq!(field, Some(&40)));
 }
 
 #[test]
@@ -114,9 +115,11 @@ fn reflect_containers() {
         },
     };
 
-    assert!(x.get_resolve_path::<usize>("container.data.field").is_err());
+    x.get_resolve_path::<usize>("container.data.field", &mut |result| {
+        assert!(result.is_err())
+    });
 
-    assert_eq!(x.get_resolve_path::<usize>("container.field"), Ok(&0));
+    x.get_resolve_path::<usize>("container.field", &mut |result| assert_eq!(result, Ok(&0)));
 
     #[derive(Reflect, Debug)]
     #[reflect(bounds = "T: Reflect")]
@@ -132,8 +135,8 @@ fn reflect_containers() {
         }),
     };
 
-    assert_eq!(b.get_resolve_path::<usize>("data.field"), Ok(&10));
-    assert!(x.get_resolve_path::<usize>("data.hidden").is_err());
+    b.get_resolve_path::<usize>("data.field", &mut |result| assert_eq!(result, Ok(&10)));
+    x.get_resolve_path::<usize>("data.hidden", &mut |result| assert!(result.is_err()));
 }
 
 #[test]
@@ -152,11 +155,11 @@ fn reflect_path() {
         e: Enum::Tuple(10),
     };
 
-    assert_eq!(hie.get_resolve_path::<usize>("s.field"), Ok(&1));
-    assert_eq!(hie.get_resolve_path::<usize>("e.Tuple@0"), Ok(&10));
+    hie.get_resolve_path::<usize>("s.field", &mut |result| assert_eq!(result, Ok(&1)));
+    hie.get_resolve_path::<usize>("e.Tuple@0", &mut |result| assert_eq!(result, Ok(&10)));
 
-    assert_eq!(hie.get_resolve_path_mut::<usize>("s.field"), Ok(&mut 1));
-    assert_eq!(hie.get_resolve_path_mut::<usize>("e.Tuple@0"), Ok(&mut 10));
+    hie.get_resolve_path_mut::<usize>("s.field", &mut |result| assert_eq!(result, Ok(&mut 1)));
+    hie.get_resolve_path_mut::<usize>("e.Tuple@0", &mut |result| assert_eq!(result, Ok(&mut 10)));
 }
 
 #[test]
@@ -164,19 +167,20 @@ fn reflect_list() {
     let mut data = vec![10usize, 11usize];
 
     let data = &mut data as &mut dyn Reflect;
-    let data = data.as_list_mut().unwrap();
+    data.as_list_mut(&mut |data| {
+        let data = data.unwrap();
+        data.get_reflect_index(0, &mut |result| assert_eq!(result, Some(&10usize)));
+        data.get_reflect_index::<usize>(2, &mut |result| assert_eq!(result, None));
 
-    assert_eq!(data.get_reflect_index(0), Some(&10usize));
-    assert_eq!(data.get_reflect_index::<usize>(2), None);
-
-    data.reflect_push(Box::new(12usize)).unwrap();
-    assert_eq!(data.get_reflect_index(2), Some(&12usize));
+        data.reflect_push(Box::new(12usize)).unwrap();
+        data.get_reflect_index(2, &mut |result| assert_eq!(result, Some(&12usize)));
+    });
 }
 
 #[test]
 fn reflect_list_path() {
     let data = vec![vec![0usize, 1], vec![2, 3, 4]];
-    assert_eq!(data.get_resolve_path("[0][1]"), Ok(&1usize));
+    data.get_resolve_path("[0][1]", &mut |result| assert_eq!(result, Ok(&1usize)));
 
     #[derive(Reflect, Debug)]
     struct X {
@@ -197,7 +201,9 @@ fn reflect_list_path() {
         ],
     };
 
-    assert_eq!(a.get_resolve_path("xs[0].data[1]"), Ok(&1usize));
+    a.get_resolve_path("xs[0].data[1]", &mut |result| {
+        assert_eq!(result, Ok(&1usize))
+    });
 }
 
 #[test]
@@ -222,9 +228,9 @@ fn reflect_custom_setter() {
     };
 
     let value = 10.0f32;
-    assert!(wrapper
-        .set_field(Wrapper::<()>::VALUE, Box::new(value))
-        .is_ok());
+    wrapper.set_field(Wrapper::<()>::VALUE, Box::new(value), &mut |result| {
+        assert!(result.is_ok())
+    });
     assert!(wrapper.is_dirty);
 }
 
@@ -241,12 +247,15 @@ fn reflect_fields_list_of_struct() {
         field_b: "Foobar".to_string(),
     };
 
-    assert_eq!(foo.fields().len(), 2);
-    assert_eq!(foo.fields()[0].downcast_ref::<f32>().cloned(), Some(1.23));
-    assert_eq!(
-        foo.fields()[1].downcast_ref::<String>().cloned(),
-        Some("Foobar".to_string())
-    );
+    foo.fields(&mut |fields| assert_eq!(fields.len(), 2));
+    foo.fields(&mut |fields| {
+        fields[0].downcast_ref::<f32>(&mut |result| assert_eq!(result.cloned(), Some(1.23)))
+    });
+    foo.fields(&mut |fields| {
+        fields[1].downcast_ref::<String>(&mut |result| {
+            assert_eq!(result.cloned(), Some("Foobar".to_string()))
+        })
+    });
 }
 
 #[test]
@@ -259,26 +268,25 @@ fn reflect_fields_list_of_enum() {
 
     let bar_variant = Foo::Bar { field_a: 1.23 };
 
-    assert_eq!(bar_variant.fields().len(), 1);
-    assert_eq!(
-        bar_variant.fields()[0].downcast_ref::<f32>().cloned(),
-        Some(1.23)
-    );
+    bar_variant.fields(&mut |fields| assert_eq!(fields.len(), 1));
+    bar_variant.fields(&mut |fields| {
+        fields[0].downcast_ref::<f32>(&mut |result| assert_eq!(result.cloned(), Some(1.23)))
+    });
 
     let baz_variant = Foo::Baz {
         field_b: 321,
         field_c: "Foobar".to_string(),
     };
 
-    assert_eq!(baz_variant.fields().len(), 2);
-    assert_eq!(
-        baz_variant.fields()[0].downcast_ref::<u32>().cloned(),
-        Some(321)
-    );
-    assert_eq!(
-        baz_variant.fields()[1].downcast_ref::<String>().cloned(),
-        Some("Foobar".to_string())
-    );
+    baz_variant.fields(&mut |fields| assert_eq!(fields.len(), 2));
+    baz_variant.fields(&mut |fields| {
+        fields[0].downcast_ref::<u32>(&mut |result| assert_eq!(result.cloned(), Some(321)))
+    });
+    baz_variant.fields(&mut |fields| {
+        fields[1].downcast_ref::<String>(&mut |result| {
+            assert_eq!(result.cloned(), Some("Foobar".to_string()))
+        })
+    });
 }
 
 fn default_prop() -> FieldInfo<'static> {
@@ -324,7 +332,7 @@ fn inspect_default() {
         },
     ];
 
-    assert_eq!(data.fields_info(), expected);
+    data.fields_info(&mut |fields_info| assert_eq!(fields_info, expected));
 }
 
 #[test]
@@ -379,7 +387,7 @@ fn inspect_attributes() {
         },
     ];
 
-    assert_eq!(data.fields_info()[0..2], expected);
+    data.fields_info(&mut |fields_info| assert_eq!(fields_info[0..2], expected));
 }
 
 #[test]
@@ -388,33 +396,36 @@ fn inspect_struct() {
     struct Tuple(f32, f32);
 
     let x = Tuple::default();
-    assert_eq!(
-        x.fields_info(),
-        vec![
-            FieldInfo {
-                owner_type_id: TypeId::of::<Tuple>(),
-                name: "0",
-                display_name: "0",
-                value: &x.0,
-                type_name: std::any::type_name::<f32>(),
-                ..default_prop()
-            },
-            FieldInfo {
-                owner_type_id: TypeId::of::<Tuple>(),
-                name: "1",
-                display_name: "1",
-                value: &x.1,
-                type_name: std::any::type_name::<f32>(),
-                ..default_prop()
-            },
-        ]
-    );
+
+    x.fields_info(&mut |fields_info| {
+        assert_eq!(
+            fields_info,
+            vec![
+                FieldInfo {
+                    owner_type_id: TypeId::of::<Tuple>(),
+                    name: "0",
+                    display_name: "0",
+                    value: &x.0,
+                    type_name: std::any::type_name::<f32>(),
+                    ..default_prop()
+                },
+                FieldInfo {
+                    owner_type_id: TypeId::of::<Tuple>(),
+                    name: "1",
+                    display_name: "1",
+                    value: &x.1,
+                    type_name: std::any::type_name::<f32>(),
+                    ..default_prop()
+                },
+            ]
+        )
+    });
 
     #[derive(Debug, Default, Reflect)]
     struct Unit;
 
     let x = Unit::default();
-    assert_eq!(x.fields_info(), vec![]);
+    x.fields_info(&mut |fields_info| assert_eq!(fields_info, vec![]));
 }
 
 #[test]
@@ -437,78 +448,82 @@ fn inspect_enum() {
         z: NonCopy { inner: 10 },
     };
 
-    assert_eq!(
-        data.fields_info(),
-        vec![
-            FieldInfo {
-                owner_type_id: TypeId::of::<Data>(),
-                name: "Named@x",
-                display_name: "X",
-                value: match data {
-                    Data::Named { ref x, .. } => x,
-                    _ => unreachable!(),
+    data.fields_info(&mut |fields_info| {
+        assert_eq!(
+            fields_info,
+            vec![
+                FieldInfo {
+                    owner_type_id: TypeId::of::<Data>(),
+                    name: "Named@x",
+                    display_name: "X",
+                    value: match data {
+                        Data::Named { ref x, .. } => x,
+                        _ => unreachable!(),
+                    },
+                    type_name: std::any::type_name::<u32>(),
+                    ..default_prop()
                 },
-                type_name: std::any::type_name::<u32>(),
-                ..default_prop()
-            },
-            FieldInfo {
-                owner_type_id: TypeId::of::<Data>(),
-                name: "Named@y",
-                display_name: "Y",
-                type_name: std::any::type_name::<u32>(),
-                value: match data {
-                    Data::Named { ref y, .. } => y,
-                    _ => unreachable!(),
+                FieldInfo {
+                    owner_type_id: TypeId::of::<Data>(),
+                    name: "Named@y",
+                    display_name: "Y",
+                    type_name: std::any::type_name::<u32>(),
+                    value: match data {
+                        Data::Named { ref y, .. } => y,
+                        _ => unreachable!(),
+                    },
+                    ..default_prop()
                 },
-                ..default_prop()
-            },
-            FieldInfo {
-                owner_type_id: TypeId::of::<Data>(),
-                name: "Named@z",
-                display_name: "Z",
-                type_name: std::any::type_name::<NonCopy>(),
-                value: match data {
-                    Data::Named { ref z, .. } => z,
-                    _ => unreachable!(),
+                FieldInfo {
+                    owner_type_id: TypeId::of::<Data>(),
+                    name: "Named@z",
+                    display_name: "Z",
+                    type_name: std::any::type_name::<NonCopy>(),
+                    value: match data {
+                        Data::Named { ref z, .. } => z,
+                        _ => unreachable!(),
+                    },
+                    ..default_prop()
                 },
-                ..default_prop()
-            },
-        ]
-    );
+            ]
+        )
+    });
 
     let data = Data::Tuple(10.0, 20.0);
 
-    assert_eq!(
-        data.fields_info(),
-        vec![
-            FieldInfo {
-                owner_type_id: TypeId::of::<Data>(),
-                name: "Tuple@0",
-                display_name: "0",
-                type_name: std::any::type_name::<f32>(),
-                value: match data {
-                    Data::Tuple(ref f0, ref _f1) => f0,
-                    _ => unreachable!(),
+    data.fields_info(&mut |fields_info| {
+        assert_eq!(
+            fields_info,
+            vec![
+                FieldInfo {
+                    owner_type_id: TypeId::of::<Data>(),
+                    name: "Tuple@0",
+                    display_name: "0",
+                    type_name: std::any::type_name::<f32>(),
+                    value: match data {
+                        Data::Tuple(ref f0, ref _f1) => f0,
+                        _ => unreachable!(),
+                    },
+                    ..default_prop()
                 },
-                ..default_prop()
-            },
-            FieldInfo {
-                owner_type_id: TypeId::of::<Data>(),
-                name: "Tuple@1",
-                display_name: "1",
-                type_name: std::any::type_name::<f32>(),
-                value: match data {
-                    Data::Tuple(ref _f0, ref f1) => f1,
-                    _ => unreachable!(),
+                FieldInfo {
+                    owner_type_id: TypeId::of::<Data>(),
+                    name: "Tuple@1",
+                    display_name: "1",
+                    type_name: std::any::type_name::<f32>(),
+                    value: match data {
+                        Data::Tuple(ref _f0, ref f1) => f1,
+                        _ => unreachable!(),
+                    },
+                    ..default_prop()
                 },
-                ..default_prop()
-            },
-        ]
-    );
+            ]
+        )
+    });
 
     // unit variants don't have fields
     let data = Data::Unit;
-    assert_eq!(data.fields_info(), vec![]);
+    data.fields_info(&mut |fields_info| assert_eq!(fields_info, vec![]));
 }
 
 #[test]
@@ -540,11 +555,26 @@ fn inspect_prop_key_constants() {
     }
 
     assert_eq!(E::TUPLE_F_0, "Tuple@0");
-    assert_eq!(E::TUPLE_F_0, E::Tuple(0).fields_info()[0].name);
+    E::Tuple(0).fields_info(&mut |fields_info| assert_eq!(E::TUPLE_F_0, fields_info[0].name));
 
     assert_eq!(E::STRUCT_FIELD, "Struct@field");
-    assert_eq!(
-        E::STRUCT_FIELD,
-        E::Struct { field: 0 }.fields_info()[0].name
-    );
+
+    E::Struct { field: 0 }
+        .fields_info(&mut |fields_info| assert_eq!(E::STRUCT_FIELD, fields_info[0].name));
+}
+
+#[test]
+fn test_reflect_mutex() {
+    let foo = Mutex::new(Struct {
+        field: 123,
+        hidden: 0,
+    });
+
+    foo.get_resolve_path::<usize>("field", &mut |result| {
+        assert_eq!(result, Ok(&123));
+    });
+
+    foo.get_resolve_path::<u32>("hidden", &mut |result| {
+        assert!(result.is_err());
+    });
 }
