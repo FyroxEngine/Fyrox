1bf76f2b6091c8996ba0316f300dabf84806757b
Dmitry <d1maxa@yandex.ru>
2023-06-04 22:50:22 +0300

    optional checkerboard background for Image widget

--- fyrox-ui/src/image.rs
+++ fyrox-ui/src/image.rs
@@ -1,13 +1,13 @@
 use crate::{
     brush::Brush,
-    core::{algebra::Vector2, color::Color, pool::Handle},
+    color::draw_checker_board,
+    core::{algebra::Vector2, color::Color, math::Rect, pool::Handle},
     define_constructor,
     draw::{CommandTexture, Draw, DrawingContext, SharedTexture},
     message::{MessageDirection, UiMessage},
     widget::{Widget, WidgetBuilder},
     BuildContext, Control, UiNode, UserInterface,
 };
-use fyrox_core::math::Rect;
 use std::{
     any::{Any, TypeId},
     ops::{Deref, DerefMut},
@@ -18,12 +18,14 @@ pub enum ImageMessage {
     Texture(Option<SharedTexture>),
     Flip(bool),
     UvRect(Rect<f32>),
+    CheckerboardBackground(bool),
 }
 
 impl ImageMessage {
     define_constructor!(ImageMessage:Texture => fn texture(Option<SharedTexture>), layout: false);
     define_constructor!(ImageMessage:Flip => fn flip(bool), layout: false);
     define_constructor!(ImageMessage:UvRect => fn uv_rect(Rect<f32>), layout: false);
+    define_constructor!(ImageMessage:CheckerboardBackground => fn checkerboard_background(bool), layout: false);
 }
 
 #[derive(Clone)]
@@ -32,6 +34,7 @@ pub struct Image {
     pub texture: Option<SharedTexture>,
     pub flip: bool,
     pub uv_rect: Rect<f32>,
+    pub checkerboard_background: bool,
 }
 
 crate::define_widget_deref!(Image);
@@ -47,45 +50,52 @@ impl Control for Image {
 
     fn draw(&self, drawing_context: &mut DrawingContext) {
         let bounds = self.widget.bounding_rect();
-        let tex_coords = if self.flip {
-            Some([
-                Vector2::new(self.uv_rect.position.x, self.uv_rect.position.y),
-                Vector2::new(
-                    self.uv_rect.position.x + self.uv_rect.size.x,
-                    self.uv_rect.position.y,
-                ),
-                Vector2::new(
-                    self.uv_rect.position.x + self.uv_rect.size.x,
-                    self.uv_rect.position.y - self.uv_rect.size.y,
-                ),
-                Vector2::new(
-                    self.uv_rect.position.x,
-                    self.uv_rect.position.y - self.uv_rect.size.y,
-                ),
-            ])
-        } else {
-            Some([
-                Vector2::new(self.uv_rect.position.x, self.uv_rect.position.y),
-                Vector2::new(
-                    self.uv_rect.position.x + self.uv_rect.size.x,
-                    self.uv_rect.position.y,
-                ),
-                Vector2::new(
-                    self.uv_rect.position.x + self.uv_rect.size.x,
-                    self.uv_rect.position.y + self.uv_rect.size.y,
-                ),
-                Vector2::new(
-                    self.uv_rect.position.x,
-                    self.uv_rect.position.y + self.uv_rect.size.y,
-                ),
-            ])
-        };
-        drawing_context.push_rect_filled(&bounds, tex_coords.as_ref());
-        let texture = self
-            .texture
-            .as_ref()
-            .map_or(CommandTexture::None, |t| CommandTexture::Texture(t.clone()));
-        drawing_context.commit(self.clip_bounds(), self.widget.background(), texture, None);
+
+        if self.checkerboard_background {
+            draw_checker_board(bounds, self.clip_bounds(), 8.0, drawing_context);
+        }
+
+        if self.texture.is_some() || !self.checkerboard_background {
+            let tex_coords = if self.flip {
+                Some([
+                    Vector2::new(self.uv_rect.position.x, self.uv_rect.position.y),
+                    Vector2::new(
+                        self.uv_rect.position.x + self.uv_rect.size.x,
+                        self.uv_rect.position.y,
+                    ),
+                    Vector2::new(
+                        self.uv_rect.position.x + self.uv_rect.size.x,
+                        self.uv_rect.position.y - self.uv_rect.size.y,
+                    ),
+                    Vector2::new(
+                        self.uv_rect.position.x,
+                        self.uv_rect.position.y - self.uv_rect.size.y,
+                    ),
+                ])
+            } else {
+                Some([
+                    Vector2::new(self.uv_rect.position.x, self.uv_rect.position.y),
+                    Vector2::new(
+                        self.uv_rect.position.x + self.uv_rect.size.x,
+                        self.uv_rect.position.y,
+                    ),
+                    Vector2::new(
+                        self.uv_rect.position.x + self.uv_rect.size.x,
+                        self.uv_rect.position.y + self.uv_rect.size.y,
+                    ),
+                    Vector2::new(
+                        self.uv_rect.position.x,
+                        self.uv_rect.position.y + self.uv_rect.size.y,
+                    ),
+                ])
+            };
+            drawing_context.push_rect_filled(&bounds, tex_coords.as_ref());
+            let texture = self
+                .texture
+                .as_ref()
+                .map_or(CommandTexture::None, |t| CommandTexture::Texture(t.clone()));
+            drawing_context.commit(self.clip_bounds(), self.widget.background(), texture, None);
+        }
     }
 
     fn handle_routed_message(&mut self, ui: &mut UserInterface, message: &mut UiMessage) {
@@ -103,6 +113,9 @@ impl Control for Image {
                     ImageMessage::UvRect(uv_rect) => {
                         self.uv_rect = *uv_rect;
                     }
+                    ImageMessage::CheckerboardBackground(value) => {
+                        self.checkerboard_background = *value;
+                    }
                 }
             }
         }
@@ -114,6 +127,7 @@ pub struct ImageBuilder {
     texture: Option<SharedTexture>,
     flip: bool,
     uv_rect: Rect<f32>,
+    checkerboard_background: bool,
 }
 
 impl ImageBuilder {
@@ -123,6 +137,7 @@ impl ImageBuilder {
             texture: None,
             flip: false,
             uv_rect: Rect::new(0.0, 0.0, 1.0, 1.0),
+            checkerboard_background: false,
         }
     }
 
@@ -146,6 +161,11 @@ impl ImageBuilder {
         self
     }
 
+    pub fn with_checkerboard_background(mut self, checkerboard_background: bool) -> Self {
+        self.checkerboard_background = checkerboard_background;
+        self
+    }
+
     pub fn build_node(mut self) -> UiNode {
         if self.widget_builder.background.is_none() {
             self.widget_builder.background = Some(Brush::Solid(Color::WHITE))
@@ -156,6 +176,7 @@ impl ImageBuilder {
             texture: self.texture,
             flip: self.flip,
             uv_rect: self.uv_rect,
+            checkerboard_background: self.checkerboard_background,
         };
         UiNode::new(image)
     }
