b033fd754e5ebc45ab83010cab7686618add9191
Dmitry <d1maxa@yandex.ru>
2023-06-18 21:23:24 +0300

    docs for `Text` widget

--- fyrox-ui/src/text.rs
+++ fyrox-ui/src/text.rs
@@ -1,3 +1,8 @@
+//! Text is a simple widget that allows you to print text on screen. See [`Text`] docs for more info and
+//! examples.
+
+#![warn(missing_docs)]
+
 use crate::{
     brush::Brush,
     core::{algebra::Vector2, color::Color, pool::Handle},
@@ -15,34 +20,299 @@ use std::{
     ops::{Deref, DerefMut},
 };
 
+/// Possible messages that can be used to alternate [`Text`] widget state at runtime.
 #[derive(Debug, Clone, PartialEq)]
 pub enum TextMessage {
+    /// Used to set new text of the widget.
     Text(String),
+    /// Used to set new text wrapping mode of the widget. See [Text](Text#text-alignment-and-word-wrapping) for usage
+    /// examples.
     Wrap(WrapMode),
+    /// Used to set new font of the widget.  See [Text](Text#fonts-and_colors) for usage examples.
     Font(SharedFont),
+    /// Used to set new vertical alignment of the widget. See [Text](Text#text-alignment-and-word-wrapping) for usage
+    /// examples.
     VerticalAlignment(VerticalAlignment),
+    /// Used to set new horizontal alignment of the widget. See [Text](Text#text-alignment-and-word-wrapping) for usage
+    /// examples.
     HorizontalAlignment(HorizontalAlignment),
+    /// Used to enable/disable shadow casting of the widget. See [Text](Text#shadows) for usage examples.
     Shadow(bool),
+    /// Used to set new dilation factor of the shadows. See [Text](Text#shadows) for usage examples.
     ShadowDilation(f32),
+    /// Used to set new brush that will be used to draw the shadows. See [Text](Text#shadows) for usage examples.
     ShadowBrush(Brush),
+    /// Used to set how much the shadows will be offset from the widget. See [Text](Text#shadows) for usage examples.
     ShadowOffset(Vector2<f32>),
 }
 
 impl TextMessage {
-    define_constructor!(TextMessage:Text => fn text(String), layout: false);
-    define_constructor!(TextMessage:Wrap=> fn wrap(WrapMode), layout: false);
-    define_constructor!(TextMessage:Font => fn font(SharedFont), layout: false);
-    define_constructor!(TextMessage:VerticalAlignment => fn vertical_alignment(VerticalAlignment), layout: false);
-    define_constructor!(TextMessage:HorizontalAlignment => fn horizontal_alignment(HorizontalAlignment), layout: false);
-    define_constructor!(TextMessage:Shadow => fn shadow(bool), layout: false);
-    define_constructor!(TextMessage:ShadowDilation => fn shadow_dilation(f32), layout: false);
-    define_constructor!(TextMessage:ShadowBrush => fn shadow_brush(Brush), layout: false);
-    define_constructor!(TextMessage:ShadowOffset => fn shadow_offset(Vector2<f32>), layout: false);
+    define_constructor!(
+        /// Creates new [`TextMessage::Text`] message.
+        TextMessage:Text => fn text(String), layout: false
+    );
+
+    define_constructor!(
+        /// Creates new [`TextMessage::Wrap`] message.
+        TextMessage:Wrap => fn wrap(WrapMode), layout: false
+    );
+
+    define_constructor!(
+        /// Creates new [`TextMessage::Font`] message.
+        TextMessage:Font => fn font(SharedFont), layout: false
+    );
+
+    define_constructor!(
+        /// Creates new [`TextMessage::VerticalAlignment`] message.
+        TextMessage:VerticalAlignment => fn vertical_alignment(VerticalAlignment), layout: false
+    );
+
+    define_constructor!(
+        /// Creates new [`TextMessage::HorizontalAlignment`] message.
+        TextMessage:HorizontalAlignment => fn horizontal_alignment(HorizontalAlignment), layout: false
+    );
+
+    define_constructor!(
+        /// Creates new [`TextMessage::Shadow`] message.
+        TextMessage:Shadow => fn shadow(bool), layout: false
+    );
+
+    define_constructor!(
+        /// Creates new [`TextMessage::ShadowDilation`] message.
+        TextMessage:ShadowDilation => fn shadow_dilation(f32), layout: false
+    );
+
+    define_constructor!(
+        /// Creates new [`TextMessage::ShadowBrush`] message.
+        TextMessage:ShadowBrush => fn shadow_brush(Brush), layout: false
+    );
+
+    define_constructor!(
+        /// Creates new [`TextMessage::ShadowOffset`] message.
+        TextMessage:ShadowOffset => fn shadow_offset(Vector2<f32>), layout: false
+    );
 }
 
+/// Text is a simple widget that allows you to print text on screen. It has various options like word wrapping, text
+/// alignment, and so on.
+///
+/// ## How to create
+///
+/// An instance of the [`Text`] widget could be created like so:
+///
+/// ```rust
+/// # use fyrox_ui::{
+/// #     core::pool::Handle,
+/// #     text::TextBuilder, widget::WidgetBuilder, UiNode, UserInterface
+/// # };
+/// fn create_text(ui: &mut UserInterface, text: &str) -> Handle<UiNode> {
+///     TextBuilder::new(WidgetBuilder::new())
+///         .with_text(text)
+///         .build(&mut ui.build_ctx())
+/// }
+/// ```
+///
+/// ## Text alignment and word wrapping
+///
+/// There are various text alignment options for both vertical and horizontal axes. Typical alignment values are:
+/// [`HorizontalAlignment::Left`], [`HorizontalAlignment::Center`], [`HorizontalAlignment::Right`] for horizontal axis,
+/// and [`VerticalAlignment::Top`], [`VerticalAlignment::Center`], [`VerticalAlignment::Bottom`] for vertical axis. An
+/// instance of centered text could be created like so:
+///
+/// ```rust,no_run
+/// # use fyrox_ui::{
+/// #     core::pool::Handle,
+/// #     text::TextBuilder, widget::WidgetBuilder, HorizontalAlignment, UiNode, UserInterface,
+/// #     VerticalAlignment,
+/// # };
+/// fn create_centered_text(ui: &mut UserInterface, text: &str) -> Handle<UiNode> {
+///     TextBuilder::new(WidgetBuilder::new())
+///         .with_horizontal_text_alignment(HorizontalAlignment::Center)
+///         .with_vertical_text_alignment(VerticalAlignment::Center)
+///     .with_text(text)
+///     .build(&mut ui.build_ctx())
+/// }
+/// ```
+///
+/// What's the difference between widget's alignment and text-specific? Widget's alignment operates on a bounding rectangle
+/// of the text and text-specific alignment operates on line-basis. This means that if you set [`HorizontalAlignment::Center`]
+/// as widget's alignment, your text lines won't be centered, instead they'll be aligned at the left and the entire text block
+/// will be aligned at center.
+///
+/// Long text is usually needs to wrap on available bounds, there are three possible options for word wrapping:
+/// [`WrapMode::NoWrap`], [`WrapMode::Letter`], [`WrapMode::Word`]. An instance of text with word-based wrapping could
+/// be created like so:
+///
+/// ```rust,no_run
+/// # use fyrox_ui::{
+/// #     core::pool::Handle,
+/// #     formatted_text::WrapMode, text::TextBuilder, widget::WidgetBuilder, UiNode,
+/// #     UserInterface,
+/// # };
+/// fn create_text_with_word_wrap(ui: &mut UserInterface, text: &str) -> Handle<UiNode> {
+///     TextBuilder::new(WidgetBuilder::new())
+///         .with_wrap(WrapMode::Word)
+///         .with_text(text)
+///         .build(&mut ui.build_ctx())
+/// }
+/// ```
+///
+/// ## Background
+///
+/// If you need to have a text with some background, you should use [`crate::border::Border`] widget as a parent widget of your
+/// text. **Caveat:** [`WidgetBuilder::with_background`] is ignored for [`Text`] widget!
+///
+/// ```rust,no_run
+/// # use fyrox_ui::{
+/// #     core::{color::Color, pool::Handle},
+/// #     border::BorderBuilder, brush::Brush, text::TextBuilder, widget::WidgetBuilder, UiNode,
+/// #     UserInterface,
+/// # };
+/// #
+/// fn create_text_with_background(ui: &mut UserInterface, text: &str) -> Handle<UiNode> {
+///     let text_widget =
+///         TextBuilder::new(WidgetBuilder::new().with_foreground(Brush::Solid(Color::RED)))
+///             .with_text(text)
+///             .build(&mut ui.build_ctx());
+///     BorderBuilder::new(
+///         WidgetBuilder::new()
+///             .with_child(text_widget) // <-- Text is now a child of the border
+///             .with_background(Brush::Solid(Color::opaque(50, 50, 50))),
+///     )
+///     .build(&mut ui.build_ctx())
+/// }
+/// ```
+///
+/// Keep in mind that now the text widget is a child widget of the border, so if you need to position the text, you should
+/// position the border, not the text.
+///
+/// ## Fonts and colors
+///
+/// To set a color of the text just use [`WidgetBuilder::with_foreground`] while building the text instance:
+///
+/// ```rust,no_run
+/// # use fyrox_ui::{
+/// #     core::{color::Color, pool::Handle},
+/// #     brush::Brush, text::TextBuilder, widget::WidgetBuilder, UiNode, UserInterface
+/// # };
+/// fn create_text(ui: &mut UserInterface, text: &str) -> Handle<UiNode> {
+///     //               vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+///     TextBuilder::new(WidgetBuilder::new().with_foreground(Brush::Solid(Color::RED)))
+///         .with_text(text)
+///         .build(&mut ui.build_ctx())
+/// }
+/// ```
+///
+/// By default, text is created with default font, however it is possible to set any custom font:
+///
+/// ```rust
+/// # use fyrox_ui::{
+/// #     core::{futures::executor::block_on, pool::Handle},
+/// #     text::TextBuilder,
+/// #     ttf::{Font, SharedFont},
+/// #     widget::WidgetBuilder,
+/// #     UiNode, UserInterface,
+/// # };
+///
+/// fn load_font() -> SharedFont {
+///     // Choose desired character set, default is Basic Latin + Latin Supplement.
+///     // Character set is a set of ranges with Unicode code points.
+///     let character_set = Font::default_char_set();
+///
+///     // Normally `block_on` should be avoided by using async.
+///     let font = block_on(Font::from_file(
+///         "path/to/your/font.ttf",
+///         24.0,
+///         character_set,
+///     ))
+///     .unwrap();
+///
+///     SharedFont::new(font)
+/// }
+///
+/// fn create_text(ui: &mut UserInterface, text: &str) -> Handle<UiNode> {
+///     TextBuilder::new(WidgetBuilder::new())
+///         .with_font(load_font())
+///         .with_text(text)
+///         .build(&mut ui.build_ctx())
+/// }
+/// ```
+///
+/// Please refer to [`crate::ttf::Font`] chapter to learn more about fonts.
+///
+/// ### Font size
+///
+/// There is no way to change font size without changing the entire font used by Text, it is known issue and there is
+/// [tracking issue](https://github.com/FyroxEngine/Fyrox/issues/74) for that. Check [`crate::ttf::Font`] docs to learn how
+/// to create fonts.
+///
+/// ## Shadows
+///
+/// Text widget supports shadows effect to add contrast to your text, which could be useful to make text readable independent
+/// on the background colors. This effect could be used for subtitles. Shadows are pretty easy to add, all you need to do
+/// is to enable them, setup desired thickness, offset and brush (solid color or gradient).
+///
+/// ```rust,no_run
+/// # use fyrox_ui::{
+/// #     core::{algebra::Vector2, color::Color, pool::Handle},
+/// #     brush::Brush, text::TextBuilder, widget::WidgetBuilder, UiNode, UserInterface
+/// # };
+/// #
+/// fn create_red_text_with_black_shadows(ui: &mut UserInterface, text: &str) -> Handle<UiNode> {
+///     TextBuilder::new(WidgetBuilder::new().with_foreground(Brush::Solid(Color::RED)))
+///         .with_text(text)
+///         // Enable shadows.
+///         .with_shadow(true)
+///         // Black shadows.
+///         .with_shadow_brush(Brush::Solid(Color::BLACK))
+///         // 1px thick.
+///         .with_shadow_dilation(1.0)
+///         // Offset the shadow slightly to the right-bottom.
+///         .with_shadow_offset(Vector2::new(1.0, 1.0))
+///         .build(&mut ui.build_ctx())
+/// }
+/// ```
+///
+/// ## Messages
+///
+/// Text widget can accept the following list of messages at runtime (respective constructors are name with small letter -
+/// `TextMessage::Text -> TextMessage::text(widget_handle, direction, text)`):
+///
+/// - [`TextMessage::Text`] - sets new text for a `Text` widget.
+/// - [`TextMessage::Wrap`] - sets new [wrapping mode](Text#text-alignment-and-word-wrapping).
+/// - [`TextMessage::Font`] - sets new [font](Text#fonts-and-colors)
+/// - [`TextMessage::VerticalAlignment`] and `TextMessage::HorizontalAlignment` sets
+/// [vertical and horizontal](Text#text-alignment-and-word-wrapping) text alignment respectively.
+/// - [`TextMessage::Shadow`] - enables or disables [shadow casting](Text#shadows)
+/// - [`TextMessage::ShadowDilation`] - sets "thickness" of the shadows under the tex.
+/// - [`TextMessage::ShadowBrush`] - sets shadow brush (allows you to change color and even make shadow with color gradients).
+/// - [`TextMessage::ShadowOffset`] - sets offset of the shadows.
+///
+/// An example of changing text at runtime could be something like this:
+///
+/// ```rust
+/// # use fyrox_ui::{
+/// #     core::pool::Handle,
+/// #     message::{MessageDirection},
+/// #     UiNode, UserInterface,
+/// #     text::TextMessage
+/// # };
+/// fn request_change_text(ui: &UserInterface, text_widget_handle: Handle<UiNode>, text: &str) {
+///     ui.send_message(TextMessage::text(
+///         text_widget_handle,
+///         MessageDirection::ToWidget,
+///         text.to_owned(),
+///     ))
+/// }
+/// ```
+///
+/// Please keep in mind, that like any other situation when you "changing" something via messages, you should remember
+/// that the change is **not** immediate.
 #[derive(Clone)]
 pub struct Text {
+    /// Base widget of the Text widget.
     pub widget: Widget,
+    /// [`FormattedText`] instance that is used to layout text and generate drawing commands.
     pub formatted_text: RefCell<FormattedText>,
 }
 
@@ -149,27 +419,33 @@ impl Control for Text {
 }
 
 impl Text {
+    /// Returns current text wrapping mode of the widget.
     pub fn wrap_mode(&self) -> WrapMode {
         self.formatted_text.borrow().wrap_mode()
     }
 
+    /// Returns current text of the widget.
     pub fn text(&self) -> String {
         self.formatted_text.borrow().text()
     }
 
+    /// Returns current font of the widget.
     pub fn font(&self) -> SharedFont {
         self.formatted_text.borrow().get_font()
     }
 
+    /// Returns current vertical alignment of the widget.
     pub fn vertical_alignment(&self) -> VerticalAlignment {
         self.formatted_text.borrow().vertical_alignment()
     }
 
+    /// Returns current horizontal alignment of the widget.
     pub fn horizontal_alignment(&self) -> HorizontalAlignment {
         self.formatted_text.borrow().horizontal_alignment()
     }
 }
 
+/// TextBuilder is used to create instances of [`Text`] widget and register them in the user interface.
 pub struct TextBuilder {
     widget_builder: WidgetBuilder,
     text: Option<String>,
@@ -184,6 +460,7 @@ pub struct TextBuilder {
 }
 
 impl TextBuilder {
+    /// Creates new [`TextBuilder`] instance using the provided base widget builder.
     pub fn new(widget_builder: WidgetBuilder) -> Self {
         Self {
             widget_builder,
@@ -199,31 +476,37 @@ impl TextBuilder {
         }
     }
 
+    /// Sets the desired text of the widget.
     pub fn with_text<P: AsRef<str>>(mut self, text: P) -> Self {
         self.text = Some(text.as_ref().to_owned());
         self
     }
 
+    /// Sets the desired font of the widget.
     pub fn with_font(mut self, font: SharedFont) -> Self {
         self.font = Some(font);
         self
     }
 
+    /// Sets the desired font of the widget using font wrapped in [`Option`].
     pub fn with_opt_font(mut self, font: Option<SharedFont>) -> Self {
         self.font = font;
         self
     }
 
+    /// Sets the desired vertical alignment of the widget.
     pub fn with_vertical_text_alignment(mut self, valign: VerticalAlignment) -> Self {
         self.vertical_text_alignment = valign;
         self
     }
 
+    /// Sets the desired horizontal alignment of the widget.
     pub fn with_horizontal_text_alignment(mut self, halign: HorizontalAlignment) -> Self {
         self.horizontal_text_alignment = halign;
         self
     }
 
+    /// Sets the desired word wrapping mode of the widget.
     pub fn with_wrap(mut self, wrap: WrapMode) -> Self {
         self.wrap = wrap;
         self
@@ -254,6 +537,7 @@ impl TextBuilder {
         self
     }
 
+    /// Finishes text widget creation and registers it in the user interface, returning its handle to you.
     pub fn build(mut self, ui: &mut BuildContext) -> Handle<UiNode> {
         let font = if let Some(font) = self.font {
             font
