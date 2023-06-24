dc11e9cbc8aa09a01eb3f9ecb3b984e62f6f1533
Dmitry <d1maxa@yandex.ru>
2023-06-24 22:49:33 +0300

    docs for `NumericUpDown` widget (partial)

--- fyrox-ui/src/numeric.rs
+++ fyrox-ui/src/numeric.rs
@@ -1,3 +1,5 @@
+//! A widget that handles numbers of any machine type. See [`NumericUpDown`] docs for more info and usage examples.
+
 use crate::{
     border::BorderBuilder,
     brush::Brush,
@@ -28,6 +30,8 @@ use std::{
     str::FromStr,
 };
 
+/// Numeric type is a trait, that has all required traits of a number type. It is used as a useful abstraction over
+/// all machine numeric types.  
 pub trait NumericType:
     NumAssign
     + FromStr
@@ -108,6 +112,89 @@ pub enum DragContext<T: NumericType> {
     },
 }
 
+/// A widget that handles numbers of any machine type. Use this widget if you need to provide input field for a numeric
+/// type.
+///
+/// ## How to create
+///
+/// Use [`NumericUpDownBuilder`] to create a new instance of the [`NumericUpDown`] widget:
+///
+/// ```rust
+/// # use fyrox_ui::{
+/// #     core::pool::Handle, numeric::NumericUpDownBuilder, widget::WidgetBuilder, BuildContext,
+/// #     UiNode,
+/// # };
+/// fn create_numeric_widget(ctx: &mut BuildContext) -> Handle<UiNode> {
+///     NumericUpDownBuilder::new(WidgetBuilder::new())
+///         .with_value(123.0f32)
+///         .build(ctx)
+/// }
+/// ```
+///
+/// Keep in mind, that this widget is generic and can work with any numeric types. Sometimes you might get an "unknown type"
+/// error message from the compiler (especially if your use `123.0` ambiguous numeric literals), in this case you need to
+/// specify the type explicitly (`NumericUpDownBuilder::<f32>::new...`).
+///
+/// ## Limits
+///
+/// This widget supports lower and upper limits for the values. It can be specified by [`NumericUpDownBuilder::with_min_value`]
+/// and [`NumericUpDownBuilder::with_max_value`] (or changed at runtime using [`NumericUpDownMessage::MinValue`] and [`NumericUpDownMessage::MaxValue`]
+/// messages):
+///
+/// ```rust
+/// use fyrox_ui::{
+///     core::pool::Handle, numeric::NumericUpDownBuilder, widget::WidgetBuilder, BuildContext,
+///     UiNode,
+/// };
+/// fn create_numeric_widget(ctx: &mut BuildContext) -> Handle<UiNode> {
+///     NumericUpDownBuilder::new(WidgetBuilder::new())
+///         .with_value(123.0f32)
+///         .with_min_value(42.0)
+///         .with_max_value(666.0)
+///         .build(ctx)
+/// }
+/// ```
+///
+/// The default limits for min and max are [`NumericType::min_value`] and [`NumericType::max_value`] respectively.
+///
+/// ## Step
+///
+/// Since the value of the widget can be changed via up/down arrow buttons (also by dragging the cursor up or down on them), the widget
+/// provides a way to set the step of the value (for increment and decrement at the same time):
+///
+/// ```rust
+/// # use fyrox_ui::{
+/// #     core::pool::Handle, numeric::NumericUpDownBuilder, widget::WidgetBuilder, BuildContext,
+/// #     UiNode,
+/// # };
+/// fn create_numeric_widget(ctx: &mut BuildContext) -> Handle<UiNode> {
+///     NumericUpDownBuilder::new(WidgetBuilder::new())
+///         .with_value(125.0f32)
+///         .with_step(5.0)
+///         .build(ctx)
+/// }
+/// ```
+///
+/// The default value of the step is [`NumericType::one`].
+///
+/// ## Precision
+///
+/// It is possible to specify **visual** rounding of the value up to desired decimal place (it does not change the way how
+/// the actual value is rounded). For example, in some cases you might get irrational values such as `1/3 ~= 0.33333333`,
+/// but you interested in only first two decimal places. In this case you can set the precision to `2`:
+///
+/// ```rust
+/// # use fyrox_ui::{
+/// #     core::pool::Handle, numeric::NumericUpDownBuilder, widget::WidgetBuilder, BuildContext,
+/// #     UiNode,
+/// # };
+/// fn create_numeric_widget(ctx: &mut BuildContext) -> Handle<UiNode> {
+///     NumericUpDownBuilder::new(WidgetBuilder::new())
+///         .with_value(0.3333333f32)
+///         .with_precision(2)
+///         .build(ctx)
+/// }
+/// ```
 #[derive(Clone)]
 pub struct NumericUpDown<T: NumericType> {
     pub widget: Widget,
