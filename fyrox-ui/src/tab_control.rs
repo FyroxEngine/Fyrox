1204c79eee9e933605666a0931d614f1a5058182
Dmitry <d1maxa@yandex.ru>
2023-06-28 18:27:23 +0300

    ability to catch the moment when the active tab of `TabControl` changed

--- fyrox-ui/src/tab_control.rs
+++ fyrox-ui/src/tab_control.rs
@@ -3,6 +3,7 @@ use crate::{
     brush::Brush,
     button::{ButtonBuilder, ButtonMessage},
     core::{color::Color, pool::Handle},
+    define_constructor,
     grid::{Column, GridBuilder, Row},
     message::{MessageDirection, UiMessage},
     widget::{Widget, WidgetBuilder, WidgetMessage},
@@ -12,6 +13,21 @@ use std::{
     any::{Any, TypeId},
     ops::{Deref, DerefMut},
 };
+
+/// A set of messages for [`TabControl`] widget.
+#[derive(Debug, Clone, PartialEq)]
+pub enum TabControlMessage {
+    /// Used to change the active tab of a [`TabControl`] widget (with [`MessageDirection::ToWidget`]) or to fetch if the active
+    /// tab has changed (with [`MessageDirection::FromWidget`]).
+    ActiveTab(Option<usize>),
+}
+
+impl TabControlMessage {
+    define_constructor!(
+        /// Creates [`TabControlMessage::ActiveTab`] message.
+        TabControlMessage:ActiveTab => fn active_tab(Option<usize>), layout: false
+    );
+}
 
 #[derive(Clone, PartialEq, Eq)]
 pub struct Tab {
@@ -23,6 +39,7 @@ pub struct Tab {
 pub struct TabControl {
     pub widget: Widget,
     pub tabs: Vec<Tab>,
+    pub active_tab: Option<usize>,
 }
 
 crate::define_widget_deref!(TabControl);
@@ -46,21 +63,34 @@ impl Control for TabControl {
     fn handle_routed_message(&mut self, ui: &mut UserInterface, message: &mut UiMessage) {
         self.widget.handle_routed_message(ui, message);
 
-        if let Some(ButtonMessage::Click) = message.data::<ButtonMessage>() {
+        if let Some(ButtonMessage::Click) = message.data() {
             for (i, tab) in self.tabs.iter().enumerate() {
                 if message.destination() == tab.header_button
                     && tab.header_button.is_some()
                     && tab.content.is_some()
                 {
-                    for (j, other_tab) in self.tabs.iter().enumerate() {
-                        ui.send_message(WidgetMessage::visibility(
-                            other_tab.content,
-                            MessageDirection::ToWidget,
-                            j == i,
-                        ));
-                    }
+                    ui.send_message(TabControlMessage::active_tab(
+                        self.handle,
+                        MessageDirection::ToWidget,
+                        Some(i),
+                    ));
                     break;
                 }
+            }
+        } else if let Some(TabControlMessage::ActiveTab(active_tab)) = message.data() {
+            if self.active_tab != *active_tab {
+                for (existing_tab_index, tab) in self.tabs.iter().enumerate() {
+                    ui.send_message(WidgetMessage::visibility(
+                        tab.content,
+                        MessageDirection::ToWidget,
+                        active_tab.map_or(false, |active_tab_index| {
+                            existing_tab_index == active_tab_index
+                        }),
+                    ));
+                }
+                self.active_tab = *active_tab;
+                // Notify potential listeners, that the active tab has changed.
+                ui.send_message(message.reverse());
             }
         }
     }
@@ -150,6 +180,7 @@ impl TabControlBuilder {
                     .build(ctx),
                 )
                 .build(),
+            active_tab: if tab_count == 0 { None } else { Some(0) },
             tabs: tab_buttons
                 .iter()
                 .zip(content)
