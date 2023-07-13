addf47a9b7ef5c7627e7d31e60417ed72ca1749e
Dmitry <d1maxa@yandex.ru>
2023-07-13 21:47:56 +0300

    `Graph::aabb_of_descendants`

--- src/scene/graph/mod.rs
+++ src/scene/graph/mod.rs
@@ -55,6 +55,7 @@ use crate::{
     script::ScriptTrait,
 };
 use fxhash::FxHashSet;
+use fyrox_core::math::aabb::AxisAlignedBoundingBox;
 use rapier3d::geometry::ColliderHandle;
 use std::{
     any::Any,
@@ -1121,6 +1122,32 @@ impl Graph {
                 child,
             );
         }
+    }
+
+    /// Tries to compute combined axis-aligned bounding box (AABB) in world-space of the hierarchy starting from the given
+    /// scene node. It will return [`None`] if the scene node handle is invalid, otherwise it will return AABB that enclosing
+    /// all the nodes in the hierarchy.
+    pub fn aabb_of_descendants(&self, root: Handle<Node>) -> Option<AxisAlignedBoundingBox> {
+        fn aabb_of_descendants_recursive(
+            graph: &Graph,
+            node: Handle<Node>,
+        ) -> Option<AxisAlignedBoundingBox> {
+            graph.try_get(node).map(|n| {
+                let mut aabb = n.local_bounding_box();
+                if aabb.is_invalid_or_degenerate() {
+                    aabb = AxisAlignedBoundingBox::collapsed().transform(&n.global_transform());
+                } else {
+                    aabb = aabb.transform(&n.global_transform());
+                }
+                for child in n.children() {
+                    if let Some(child_aabb) = aabb_of_descendants_recursive(graph, *child) {
+                        aabb.add_box(child_aabb);
+                    }
+                }
+                aabb
+            })
+        }
+        aabb_of_descendants_recursive(self, root)
     }
 
     /// Calculates local and global transform, global visibility for each node in graph starting from the
