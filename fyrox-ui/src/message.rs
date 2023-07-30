35e735055497c00f5af5588d54b8fa91da6e8a4b
Dmitry <d1maxa@yandex.ru>
2023-07-30 21:43:46 +0300

    improved docs and added examples for ui message

--- fyrox-ui/src/message.rs
+++ fyrox-ui/src/message.rs
@@ -1,17 +1,5 @@
-//! Message and events module contains all possible widget messages and OS events.
-//!
-//! This UI library uses message passing mechanism to communicate with widgets.
-//! This is very simple and reliable mechanism that effectively decouples widgets
-//! from each other. There is no direct way of modify something during runtime,
-//! you have to use messages to change state of ui elements.
-//!
-//! # Direction
-//!
-//! Each message marked with "Direction" field, which means supported routes for
-//! message. For example [ButtonMessage::Click](enum.ButtonMessage.html) has "Direction: To/From UI" which
-//! means that it can be sent either from internals of library or from user code.
-//! However [WidgetMessage::GotFocus](enum.WidgetMessage.html) has "Direction: From UI" which means that only
-//! internal library code can send such messages without a risk of breaking anything.
+//! Message and events module contains all possible widget messages and OS events. See [`UiMessage`] docs for more info and
+//! examples.
 
 use crate::{
     core::{algebra::Vector2, pool::Handle, reflect::prelude::*},
@@ -21,6 +9,112 @@ use serde::{Deserialize, Serialize};
 use std::{any::Any, cell::Cell, fmt::Debug, rc::Rc};
 use strum_macros::{AsRefStr, EnumString, EnumVariantNames};
 
+mod kek {
+    use crate::{
+        core::pool::Handle, define_constructor, message::MessageDirection, UiMessage, UiNode,
+        UserInterface,
+    };
+
+    // Message must be debuggable and comparable.
+    #[derive(Debug, PartialEq)]
+    enum MyWidgetMessage {
+        DoSomething,
+        Foo(u32),
+        Bar { foo: u32, baz: u8 },
+    }
+
+    impl MyWidgetMessage {
+        define_constructor!(MyWidgetMessage:DoSomething => fn do_something(), layout: false);
+        define_constructor!(MyWidgetMessage:Foo => fn foo(u32), layout: false);
+        define_constructor!(MyWidgetMessage:Bar => fn bar(foo: u32, baz: u8), layout: false);
+    }
+
+    fn using_messages(my_widget: Handle<UiNode>, ui: &UserInterface) {
+        // Send MyWidgetMessage::DoSomething
+        ui.send_message(MyWidgetMessage::do_something(
+            my_widget,
+            MessageDirection::ToWidget,
+        ));
+        // Send MyWidgetMessage::Foo
+        ui.send_message(MyWidgetMessage::foo(
+            my_widget,
+            MessageDirection::ToWidget,
+            5,
+        ));
+        // Send MyWidgetMessage::Bar
+        ui.send_message(MyWidgetMessage::bar(
+            my_widget,
+            MessageDirection::ToWidget,
+            1,
+            2,
+        ));
+    }
+}
+
+/// Defines a new message constructor for a enum variant. It is widely used in this crate to create shortcuts to create
+/// messages. Why is it needed anyway? Just to reduce boilerplate code as much as possible.
+///
+/// ## Examples
+///
+/// The following example shows how to create message constructors for various kinds of enum variants:
+///
+/// ```rust
+/// # use fyrox_ui::{
+/// #     core::pool::Handle, define_constructor, message::MessageDirection, UiMessage, UiNode,
+/// #     UserInterface,
+/// # };
+/// #
+/// // Message must be debuggable and comparable.
+/// #[derive(Debug, PartialEq)]
+/// enum MyWidgetMessage {
+///     DoSomething,
+///     Foo(u32),
+///     Bar { foo: u32, baz: u8 },
+/// }
+///
+/// impl MyWidgetMessage {
+///     // The first option is used to create constructors plain enum variants:
+///     //
+///     //                  enum name       variant            name          perform layout?
+///     //                      v              v                 v                  v
+///     define_constructor!(MyWidgetMessage:DoSomething => fn do_something(), layout: false);
+///
+///     // The second option is used to create constructors for single-arg tuple enum variants:
+///     //
+///     //                  enum name     variant    name arg    perform layout?
+///     //                      v            v         v   v           v
+///     define_constructor!(MyWidgetMessage:Foo => fn foo(u32), layout: false);
+///
+///     // The third option is used to create constructors for enum variants with fields:
+///     //
+///     //                  enum name     variant    name arg  type arg type  perform layout?
+///     //                      v            v         v   v     v   v    v          v
+///     define_constructor!(MyWidgetMessage:Bar => fn bar(foo: u32, baz: u8), layout: false);
+/// }
+///
+/// fn using_messages(my_widget: Handle<UiNode>, ui: &UserInterface) {
+///     // Send MyWidgetMessage::DoSomething
+///     ui.send_message(MyWidgetMessage::do_something(
+///         my_widget,
+///         MessageDirection::ToWidget,
+///     ));
+///
+///     // Send MyWidgetMessage::Foo
+///     ui.send_message(MyWidgetMessage::foo(
+///         my_widget,
+///         MessageDirection::ToWidget,
+///         5,
+///     ));
+///
+///     // Send MyWidgetMessage::Bar
+///     ui.send_message(MyWidgetMessage::bar(
+///         my_widget,
+///         MessageDirection::ToWidget,
+///         1,
+///         2,
+///     ));
+/// }
+/// ```
 #[macro_export]
 macro_rules! define_constructor {
     ($(#[$meta:meta])* $inner:ident : $inner_var:tt => fn $name:ident(), layout: $perform_layout:expr) => {
@@ -69,21 +163,16 @@ macro_rules! define_constructor {
     }
 }
 
-/// Message direction allows you to distinguish from where message has came from.
-/// Often there is a need to find out who created a message to respond properly.
-/// Imagine that we have a NumericUpDown input field for a property and we using
-/// some data source to feed data into input field. When we change something in
-/// the input field by typing, it creates a message with new value. On other
-/// hand we often need to put new value in the input field from some code, in this
-/// case we again creating a message. But how to understand from which "side"
-/// message has came from? Was it filled in by user and we should create a command
-/// to change value in the data source, or it was created from syncing code just to
-/// pass new value to UI? This problem solved by setting a direction to a message.
-/// Also it solves another problem: often we need to respond to a message only if
-/// it did some changes. In this case at first we fire a message with ToWidget
-/// direction, widget catches it and checks if changes are needed and if so, it
-/// "rethrows" message with direction FromWidget. Listeners are "subscribed" to
-/// FromWidget messages only and won't respond to ToWidget messages.
+/// Message direction allows you to distinguish from where message has came from. Often there is a need to find out who
+/// created a message to respond properly. Imagine that we have a NumericUpDown input field for a property and we using
+/// some data source to feed data into input field. When we change something in the input field by typing, it creates a
+/// message with new value. On other hand we often need to put new value in the input field from some code, in this case
+/// we again creating a message. But how to understand from which "side" message has came from? Was it filled in by user
+/// and we should create a command  to change value in the data source, or it was created from syncing code just to pass
+/// new value to UI? This problem solved by setting a direction to a message. Also it solves another problem: often we
+/// need to respond to a message only if it did some changes. In this case at first we fire a message with ToWidget direction,
+/// widget catches it and checks if changes are needed and if so, it "rethrows" message with direction FromWidget. Listeners
+/// are "subscribed" to FromWidget messages only and won't respond to ToWidget messages.
 #[derive(Debug, Copy, Clone, PartialOrd, PartialEq, Hash, Eq)]
 pub enum MessageDirection {
     /// Used to indicate a request for changes in a widget.
@@ -126,40 +215,99 @@ where
     }
 }
 
-/// Message is basic communication element that is used to deliver information to UI nodes
-/// or to user code.
+/// Message is basic communication element that is used to deliver information to widget or to user code.
 ///
-/// # Threading
+/// ## Motivation
+///
+/// This UI library uses message passing mechanism to communicate with widgets. This is very simple and reliable mechanism that
+/// effectively decouples widgets from each other. There is no direct way of modify something during runtime, you have to use
+/// messages to change state of ui elements.
+///
+/// ## Direction
+///
+/// Each message marked with "Direction" field, which means supported routes for message. For example [`crate::button::ButtonMessage::Click`]
+/// has "Direction: To/From UI" which means that it can be sent either from internals of library or from user code. However
+/// [`crate::widget::WidgetMessage::Focus`] has "Direction: From UI" which means that only internal library code can send such messages without
+/// a risk of breaking anything.
+///
+/// ## Threading
 ///
 /// UiMessage is nor Send or Sync. User interface is a single-thread thing, as well as its messages.
+///
+/// ## Examples
+///
+/// ```rust
+/// use fyrox_ui::{
+///     core::pool::Handle, define_constructor, message::MessageDirection, UiMessage, UiNode,
+///     UserInterface,
+/// };
+///
+/// // Message must be debuggable and comparable.
+/// #[derive(Debug, PartialEq)]
+/// enum MyWidgetMessage {
+///     DoSomething,
+///     Foo(u32),
+///     Bar { foo: u32, baz: u8 },
+/// }
+///
+/// impl MyWidgetMessage {
+///     define_constructor!(MyWidgetMessage:DoSomething => fn do_something(), layout: false);     
+///     define_constructor!(MyWidgetMessage:Foo => fn foo(u32), layout: false);
+///     define_constructor!(MyWidgetMessage:Bar => fn bar(foo: u32, baz: u8), layout: false);
+/// }
+///
+/// fn using_messages(my_widget: Handle<UiNode>, ui: &UserInterface) {
+///     // Send MyWidgetMessage::DoSomething
+///     ui.send_message(MyWidgetMessage::do_something(
+///         my_widget,
+///         MessageDirection::ToWidget,
+///     ));
+///     // Send MyWidgetMessage::Foo
+///     ui.send_message(MyWidgetMessage::foo(
+///         my_widget,
+///         MessageDirection::ToWidget,
+///         5,
+///     ));
+///     // Send MyWidgetMessage::Bar
+///     ui.send_message(MyWidgetMessage::bar(
+///         my_widget,
+///         MessageDirection::ToWidget,
+///         1,
+///         2,
+///     ));
+/// }
+/// ```
+///
+///
 #[derive(Debug, Clone)]
 pub struct UiMessage {
-    /// Useful flag to check if a message was already handled.
+    /// Useful flag to check if a message was already handled. It could be used to mark messages as "handled" to prevent
+    /// any further responses to them. It is especially useful in bubble message routing, when a message is passed through
+    /// the entire chain of parent nodes starting from current. In this, you can mark a message as "handled" and also check
+    /// if it is handled or not. For example, this is used in [`crate::tree::Tree`] implementation, to prevent double-click
+    /// to close all the parent trees from current.
     pub handled: Cell<bool>,
 
-    /// Actual message data.
+    /// Actual message data. Use [`UiMessage::data`] method to try to downcast the internal data to a specific type.
     pub data: Rc<dyn MessageData>,
 
-    /// Handle of node that will receive message. Please note that all nodes in hierarchy will
-    /// also receive this message, order is "up-on-tree".
+    /// Handle of node that will receive message. Please note that **all** nodes in hierarchy will also receive this message,
+    /// order is "up-on-tree" (so called "bubble" message routing). T
     pub destination: Handle<UiNode>,
 
-    /// Indicates the direction of the message.
-    ///
-    /// See [MessageDirection](enum.MessageDirection.html) for details.
+    /// Indicates the direction of the message. See [`MessageDirection`] docs for more info.
     pub direction: MessageDirection,
 
     /// Whether or not message requires layout to be calculated first.
     ///
-    /// Some of message handling routines uses layout info, but message loop
-    /// performed right after layout pass, but some of messages may change
-    /// layout and this flag tells UI to perform layout before passing message
-    /// further. In ideal case we'd perform layout after **each** message, but
-    /// since layout pass is super heavy we should do it **only** when it is
-    /// actually needed.
+    /// ## Motivation
+    ///
+    /// Some of message handling routines uses layout info, but message loop performed right after layout pass, but some of messages
+    /// may change layout and this flag tells UI to perform layout before passing message further. In ideal case we'd perform layout
+    /// after **each** message, but since layout pass is super heavy we should do it **only** when it is actually needed.
     pub perform_layout: Cell<bool>,
 
-    /// A custom user flags.
+    /// A custom user flags. Use it if `handled` flag is not enough.
     pub flags: u64,
 }
 
@@ -206,15 +354,11 @@ impl UiMessage {
         self
     }
 
-    /// Creates a new copy of the message with reversed direction. Typical use case is
-    /// to re-send messages to create "response" in widget. For example you have a float
-    /// input field and it has Value message. When the input field receives Value message
-    /// with [MessageDirection::ToWidget](enum.MessageDirection.html#variant.ToWidget)
-    /// it checks if value needs to be changed and if it does, it re-sends same message
-    /// but with reversed direction back to message queue so every "listener" can reach
-    /// properly. The input field won't react at
-    /// [MessageDirection::FromWidget](enum.MessageDirection.html#variant.FromWidget)
-    /// message so there will be no infinite message loop.
+    /// Creates a new copy of the message with reversed direction. Typical use case is to re-send messages to create "response"
+    /// in a widget. For example you have a float input field and it has Value message. When the input field receives Value message
+    /// with [`MessageDirection::ToWidget`] it checks if value needs to be changed and if it does, it re-sends same message, but with
+    /// reversed direction back to message queue so every "listener" can reach properly. The input field won't react at
+    /// [`MessageDirection::FromWidget`] message so there will be no infinite message loop.
     #[must_use = "method creates new value which must be used"]
     pub fn reverse(&self) -> Self {
         Self {
