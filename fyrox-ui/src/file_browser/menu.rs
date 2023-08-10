c8173beedb6c35b244948997561bd231d3a992b1
Dmitry <d1maxa@yandex.ru>
2023-08-10 23:03:59 +0300

    show confirmation message box when deleting fs item

--- fyrox-ui/src/file_browser/menu.rs
+++ fyrox-ui/src/file_browser/menu.rs
@@ -3,13 +3,16 @@ use crate::{
     draw::DrawingContext,
     menu::{MenuItemBuilder, MenuItemContent, MenuItemMessage},
     message::{MessageDirection, OsEvent, UiMessage},
+    messagebox::{MessageBoxBuilder, MessageBoxButtons, MessageBoxMessage, MessageBoxResult},
     popup::{Placement, Popup, PopupBuilder, PopupMessage},
     stack_panel::StackPanelBuilder,
     widget::{Widget, WidgetBuilder, WidgetMessage},
+    window::{WindowBuilder, WindowTitle},
     BuildContext, Control, NodeHandleMapping, Thickness, UiNode, UserInterface,
 };
 use std::{
     any::{Any, TypeId},
+    cell::Cell,
     ops::{Deref, DerefMut},
     path::{Path, PathBuf},
     sync::mpsc::Sender,
@@ -20,7 +23,7 @@ pub struct ItemContextMenu {
     pub popup: Popup,
     pub delete: Handle<UiNode>,
     pub make_folder: Handle<UiNode>,
-    pub delete_message_box: Handle<UiNode>,
+    pub delete_message_box: Cell<Handle<UiNode>>,
 }
 
 impl Deref for ItemContextMenu {
@@ -88,11 +91,25 @@ impl Control for ItemContextMenu {
         } else if let Some(MenuItemMessage::Click) = message.data() {
             if message.destination() == self.delete {
                 if let Some(item_path) = self.item_path(ui) {
-                    if item_path.is_dir() {
-                        Log::verify(std::fs::remove_dir_all(item_path));
-                    } else {
-                        Log::verify(std::fs::remove_file(item_path));
-                    }
+                    self.delete_message_box.set(
+                        MessageBoxBuilder::new(
+                            WindowBuilder::new(
+                                WidgetBuilder::new().with_width(250.0).with_height(100.0),
+                            )
+                            .with_title(WindowTitle::text("Confirm Action"))
+                            .open(false),
+                        )
+                        .with_text(format!("Delete {} file?", item_path.display()).as_str())
+                        .with_buttons(MessageBoxButtons::YesNo)
+                        .build(&mut ui.build_ctx()),
+                    );
+
+                    ui.send_message(MessageBoxMessage::open(
+                        self.delete_message_box.get(),
+                        MessageDirection::ToWidget,
+                        None,
+                        None,
+                    ));
                 }
             } else if message.destination() == self.make_folder {
                 // TODO
@@ -101,7 +118,28 @@ impl Control for ItemContextMenu {
     }
 
     fn preview_message(&self, ui: &UserInterface, message: &mut UiMessage) {
-        self.popup.preview_message(ui, message)
+        self.popup.preview_message(ui, message);
+
+        if let Some(MessageBoxMessage::Close(result)) = message.data() {
+            if message.destination() == self.delete_message_box.get() {
+                if let MessageBoxResult::Yes = *result {
+                    if let Some(item_path) = self.item_path(ui) {
+                        if item_path.is_dir() {
+                            Log::verify(std::fs::remove_dir_all(item_path));
+                        } else {
+                            Log::verify(std::fs::remove_file(item_path));
+                        }
+                    }
+                }
+
+                ui.send_message(WidgetMessage::remove(
+                    self.delete_message_box.get(),
+                    MessageDirection::ToWidget,
+                ));
+
+                self.delete_message_box.set(Handle::NONE);
+            }
+        }
     }
 
     fn handle_os_event(
@@ -118,31 +156,35 @@ impl ItemContextMenu {
     pub fn build(ctx: &mut BuildContext) -> Handle<UiNode> {
         let delete;
         let make_folder;
-        let popup = PopupBuilder::new(WidgetBuilder::new().with_visibility(false))
-            .with_content(
-                StackPanelBuilder::new(
-                    WidgetBuilder::new()
-                        .with_width(120.0)
-                        .with_child({
-                            delete = MenuItemBuilder::new(
-                                WidgetBuilder::new().with_margin(Thickness::uniform(2.0)),
-                            )
-                            .with_content(MenuItemContent::text("Delete"))
-                            .build(ctx);
-                            delete
-                        })
-                        .with_child({
-                            make_folder = MenuItemBuilder::new(
-                                WidgetBuilder::new().with_margin(Thickness::uniform(2.0)),
-                            )
-                            .with_content(MenuItemContent::text("Make Folder"))
-                            .build(ctx);
-                            make_folder
-                        }),
-                )
-                .build(ctx),
+        let popup = PopupBuilder::new(
+            WidgetBuilder::new()
+                .with_preview_messages(true)
+                .with_visibility(false),
+        )
+        .with_content(
+            StackPanelBuilder::new(
+                WidgetBuilder::new()
+                    .with_width(120.0)
+                    .with_child({
+                        delete = MenuItemBuilder::new(
+                            WidgetBuilder::new().with_margin(Thickness::uniform(2.0)),
+                        )
+                        .with_content(MenuItemContent::text("Delete"))
+                        .build(ctx);
+                        delete
+                    })
+                    .with_child({
+                        make_folder = MenuItemBuilder::new(
+                            WidgetBuilder::new().with_margin(Thickness::uniform(2.0)),
+                        )
+                        .with_content(MenuItemContent::text("Make Folder"))
+                        .build(ctx);
+                        make_folder
+                    }),
             )
-            .build_popup(ctx);
+            .build(ctx),
+        )
+        .build_popup(ctx);
 
         let menu = Self {
             popup,
