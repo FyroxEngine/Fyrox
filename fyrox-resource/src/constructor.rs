8cdd856ad715162452fa4d18355dcae578d01d8f
san-smith <san-smith@mail.ru>
2023-09-09 02:57:30 +0700

    test: ResourceConstructorContainer

--- fyrox-resource/src/constructor.rs
+++ fyrox-resource/src/constructor.rs
@@ -63,3 +63,112 @@ impl ResourceConstructorContainer {
         self.len() == 0
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::path::PathBuf;
+
+    use fyrox_core::reflect::prelude::*;
+    use fyrox_core::visitor::{Visit, VisitResult, Visitor};
+
+    use super::*;
+
+    #[derive(Debug, Default, Reflect, Visit)]
+    struct Stub {}
+
+    impl ResourceData for Stub {
+        fn path(&self) -> std::borrow::Cow<std::path::Path> {
+            unimplemented!()
+        }
+
+        fn set_path(&mut self, _path: std::path::PathBuf) {
+            unimplemented!()
+        }
+
+        fn as_any(&self) -> &dyn std::any::Any {
+            unimplemented!()
+        }
+
+        fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+            unimplemented!()
+        }
+
+        fn type_uuid(&self) -> Uuid {
+            unimplemented!()
+        }
+    }
+
+    impl TypeUuidProvider for Stub {
+        fn type_uuid() -> Uuid {
+            Uuid::default()
+        }
+    }
+
+    #[test]
+    fn resource_constructor_container_new() {
+        let c = ResourceConstructorContainer::new();
+
+        assert_eq!(c.len(), 0);
+
+        c.add::<Stub>();
+        assert_eq!(c.len(), 1);
+    }
+
+    #[test]
+    fn resource_constructor_container_add_custom() {
+        let c = ResourceConstructorContainer::new();
+
+        assert!(c.is_empty());
+
+        c.add_custom(Uuid::default(), Box::new(|| Box::new(Stub {})));
+        assert_eq!(c.len(), 1);
+
+        c.remove(Uuid::default());
+        assert!(c.is_empty());
+    }
+
+    #[test]
+    fn resource_constructor_container_try_create() {
+        let c = ResourceConstructorContainer::new();
+        c.add::<Stub>();
+
+        let res = c.try_create(&Uuid::default());
+        assert!(res.is_some());
+    }
+
+    #[test]
+    #[should_panic]
+    fn stub_path() {
+        let s = Stub {};
+        s.path();
+    }
+
+    #[test]
+    #[should_panic]
+    fn stub_set_path() {
+        let mut s = Stub {};
+        s.set_path(PathBuf::new());
+    }
+
+    #[test]
+    #[should_panic]
+    fn stub_set_as_any() {
+        let s = Stub {};
+        ResourceData::as_any(&s);
+    }
+
+    #[test]
+    #[should_panic]
+    fn stub_set_as_any_mut() {
+        let mut s = Stub {};
+        ResourceData::as_any_mut(&mut s);
+        s.type_uuid();
+    }
+
+    #[test]
+    #[should_panic]
+    fn stub_set_type_uuid() {
+        let s = Stub {};
+        s.type_uuid();
+    }
+}
