2bf397f41e72c9d9e02d205392981853a8b17815
Dmitry <d1maxa@yandex.ru>
2023-09-25 13:49:19 +0300

    removed obsolete examples
    - replaced with demo projects - https://github.com/FyroxEngine/Fyrox-demo-projects

--- examples/scripting/executor/src/main.rs
+++
@@ -1,8 +0,0 @@
-use fyrox::engine::executor::Executor;
-use game::GameConstructor;
-
-fn main() {
-    let mut executor = Executor::new();
-    executor.add_plugin_constructor(GameConstructor);
-    executor.run()
-}
