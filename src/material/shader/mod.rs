710b43ba120ac32acc36e5a75ba3e45ca68c12c6
Dmitry Stepanov <d1maxa@yandex.ru>
2023-11-09 18:59:42 +0300

    fixed material docs

--- src/material/shader/mod.rs
+++ src/material/shader/mod.rs
@@ -146,21 +146,25 @@
 //! - PointShadow - A pass that emits distance from a fragment to a point light, later this depth
 //! map will be used to render shadows.
 //!
-//! # Built-in variables
+//! ## Built-in properties
 //!
-//! There are number of build-in variables that Fyrox pass to each shader automatically:
+//! There are number of built-in properties, that Fyrox will try to assign automatically if they're defined
+//! in your shader:
 //!
-//! | Name                      | Type            | Description
-//! |---------------------------|-----------------|--------------------------------------------
-//! | fyrox_worldMatrix          | `Matrix4`       | Local-to-world transformation.
-//! | fyrox_worldViewProjection  | `Matrix4`       | Local-to-clip-space transform.
-//! | fyrox_boneMatrices         | `[Matrix4; 60]` | Array of bone matrices.
-//! | fyrox_useSkeletalAnimation | `Vector3`       | Whether skinned meshes is rendering or not.
-//! | fyrox_cameraPosition       | `Vector3`       | Position of the camera.
-//! | fyrox_usePOM               | `bool`          | Whether to use parallax mapping or not.
-//! | fyrox_lightPosition        | `Vector3`       | Light position.
+//! | Name                       | Type         | Description                                                                                                       |
+//! |----------------------------|--------------|-------------------------------------------------------------------------------------------------------------------|
+//! | fyrox_worldMatrix          | `mat4`       | Local-to-world transformation.                                                                                    |
+//! | fyrox_worldViewProjection  | `mat4`       | Local-to-clip-space transform.                                                                                    |
+//! | fyrox_boneMatrices         | `sampler2D`  | Array of bone matrices packed into a texture. Use `S_FetchMatrix` built-in method to fetch a matrix by its index. |
+//! | fyrox_useSkeletalAnimation | `bool`       | Whether skinned meshes is rendering or not.                                                                       |
+//! | fyrox_cameraPosition       | `vec3`       | Position of the camera.                                                                                           |
+//! | fyrox_usePOM               | `bool`       | Whether to use parallax mapping or not.                                                                           |
+//! | fyrox_lightPosition        | `vec3`       | Light position.                                                                                                   |
+//! | fyrox_blendShapesStorage   | `sampler3D`  | 3D texture of layered blend shape storage. Use `S_FetchBlendShapeOffsets` built-in method to fetch info.          |
+//! | fyrox_blendShapesWeights   | `float[128]` | Weights of all available blend shapes.                                                                            |
+//! | fyrox_blendShapesCount     | `int`        | Total amount of blend shapes.                                                                                     |
 //!
-//! To use any of the variables, just define a uniform with appropriate name:
+//! To use any of the properties, just define a uniform with an appropriate name:
 //!
 //! ```glsl
 //! uniform mat4 fyrox_worldMatrix;
