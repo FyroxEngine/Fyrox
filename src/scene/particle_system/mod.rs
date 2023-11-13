86f5c3aa27b28291fc0b5aae6b1e08e968370a12
Dmitry Stepanov <d1maxa@yandex.ru>
2023-11-13 21:55:37 +0300

    fixed doc tests

--- src/scene/particle_system/mod.rs
+++ src/scene/particle_system/mod.rs
@@ -132,48 +132,75 @@ impl Visit for ParticleSystemRng {
 /// Simple smoke effect can be create like so:
 ///
 /// ```
-/// use fyrox::scene::particle_system::{
-///     emitter::sphere::SphereEmitter, ParticleSystemBuilder, emitter::Emitter,
-///     emitter::base::BaseEmitterBuilder, emitter::sphere::SphereEmitterBuilder
-/// };
-/// use fyrox::asset::manager::ResourceManager;
-/// use fyrox::core::algebra::Vector3;
-/// use fyrox::scene::graph::Graph;
-/// use fyrox::scene::node::Node;
-/// use fyrox::scene::transform::TransformBuilder;
-/// use fyrox::core::color_gradient::{GradientPoint, ColorGradient};
-/// use fyrox::scene::base::BaseBuilder;
-/// use fyrox::core::color::Color;
-/// use std::path::Path;
-/// use fyrox::resource::texture::{Texture, TexturePixelKind};
-///
+/// # use fyrox::{
+/// #     asset::manager::ResourceManager,
+/// #     core::{
+/// #         algebra::Vector3,
+/// #         color::Color,
+/// #         color_gradient::{ColorGradient, GradientPoint},
+/// #         sstorage::ImmutableString,
+/// #     },
+/// #     material::{Material, PropertyValue, SharedMaterial},
+/// #     resource::texture::Texture,
+/// #     scene::{
+/// #         base::BaseBuilder,
+/// #         graph::Graph,
+/// #         particle_system::{
+/// #             emitter::base::BaseEmitterBuilder, emitter::sphere::SphereEmitterBuilder,
+/// #             ParticleSystemBuilder,
+/// #         },
+/// #         transform::TransformBuilder,
+/// #     },
+/// # };
+/// # use std::path::Path;
+/// #
 /// fn create_smoke(graph: &mut Graph, resource_manager: &mut ResourceManager, pos: Vector3<f32>) {
-///      ParticleSystemBuilder::new(BaseBuilder::new()
-///         .with_lifetime(5.0)
-///         .with_local_transform(TransformBuilder::new()
-///             .with_local_position(pos)
-///             .build()))
-///         .with_acceleration(Vector3::new(0.0, 0.0, 0.0))
-///         .with_color_over_lifetime_gradient({
-///             let mut gradient = ColorGradient::new();
-///             gradient.add_point(GradientPoint::new(0.00, Color::from_rgba(150, 150, 150, 0)));
-///             gradient.add_point(GradientPoint::new(0.05, Color::from_rgba(150, 150, 150, 220)));
-///             gradient.add_point(GradientPoint::new(0.85, Color::from_rgba(255, 255, 255, 180)));
-///             gradient.add_point(GradientPoint::new(1.00, Color::from_rgba(255, 255, 255, 0)));
-///             gradient
-///         })
-///         .with_emitters(vec![
-///             SphereEmitterBuilder::new(BaseEmitterBuilder::new()
-///                 .with_max_particles(100)
-///                 .with_spawn_rate(50)
-///                 .with_x_velocity_range(-0.01..0.01)
-///                 .with_y_velocity_range(0.02..0.03)
-///                 .with_z_velocity_range(-0.01..0.01))
-///                 .with_radius(0.01)
-///                 .build()
-///         ])
-///         .with_texture(resource_manager.request::<Texture, _>(Path::new("data/particles/smoke_04.tga")))
-///         .build(graph);
+///     let mut material = Material::standard_particle_system();
+///     material
+///         .set_property(
+///             &ImmutableString::new("diffuseTexture"),
+///             PropertyValue::Sampler {
+///                 value: Some(
+///                     resource_manager
+///                         .request::<Texture, _>(Path::new("data/particles/smoke_04.tga")),
+///                 ),
+///                 fallback: Default::default(),
+///             },
+///         )
+///         .unwrap();
+///
+///     ParticleSystemBuilder::new(
+///         BaseBuilder::new()
+///             .with_lifetime(5.0)
+///             .with_local_transform(TransformBuilder::new().with_local_position(pos).build()),
+///     )
+///     .with_acceleration(Vector3::new(0.0, 0.0, 0.0))
+///     .with_color_over_lifetime_gradient({
+///         let mut gradient = ColorGradient::new();
+///         gradient.add_point(GradientPoint::new(0.00, Color::from_rgba(150, 150, 150, 0)));
+///         gradient.add_point(GradientPoint::new(
+///             0.05,
+///             Color::from_rgba(150, 150, 150, 220),
+///         ));
+///         gradient.add_point(GradientPoint::new(
+///             0.85,
+///             Color::from_rgba(255, 255, 255, 180),
+///         ));
+///         gradient.add_point(GradientPoint::new(1.00, Color::from_rgba(255, 255, 255, 0)));
+///         gradient
+///     })
+///     .with_emitters(vec![SphereEmitterBuilder::new(
+///         BaseEmitterBuilder::new()
+///             .with_max_particles(100)
+///             .with_spawn_rate(50)
+///             .with_x_velocity_range(-0.01..0.01)
+///             .with_y_velocity_range(0.02..0.03)
+///             .with_z_velocity_range(-0.01..0.01),
+///     )
+///     .with_radius(0.01)
+///     .build()])
+///     .with_material(SharedMaterial::new(material))
+///     .build(graph);
 /// }
 /// ```
 #[derive(Debug, Visit, Clone, Reflect)]
