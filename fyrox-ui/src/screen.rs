dbb6b755823479c89ff4551a570ead97ab1066b1
Dmitry Stepanov <d1maxa@yandex.ru>
2023-12-23 23:20:41 +0300

    docs for screen widget

--- fyrox-ui/src/screen.rs
+++ fyrox-ui/src/screen.rs
@@ -1,3 +1,8 @@
+//! Screen is a widget that always has the size of the screen of the UI in which it is used. See
+//! docs for [`Screen`] for more info and usage examples.
+
+#![warn(missing_docs)]
+
 use crate::{
     core::{
         algebra::Vector2, math::Rect, pool::Handle, reflect::prelude::*, uuid_provider,
@@ -14,10 +19,81 @@ use std::{
     sync::mpsc::Sender,
 };
 
+/// Screen is a widget that always has the size of the screen of the UI in which it is used. It is
+/// main use case is to provide automatic layout functionality, that will always provide screen size
+/// to its children widgets. This is needed, because the root node of any UI is [`crate::canvas::Canvas`]
+/// which provides infinite bounds as a layout constraint, thus making it impossible for automatic
+/// fitting to the current screen size. For example, Screen widget could be used as a root node for
+/// [`crate::grid::Grid`] widget - in this case the grid instance will always have the size of the
+/// screen and will automatically shrink or expand when the screen size changes. It is ideal choice if
+/// you want to have some widgets always centered on screen (for example - crosshair, main menu of
+/// your game, etc.).
+///
+/// ## Example
+///
+/// The following examples creates a simple main menu of a game with just two buttons. The buttons
+/// will always be centered in the current screen bounds.
+///
+/// ```rust
+/// use fyrox_ui::{
+///     core::pool::Handle,
+///     button::ButtonBuilder,
+///     grid::{Column, GridBuilder, Row},
+///     screen::ScreenBuilder,
+///     stack_panel::StackPanelBuilder,
+///     widget::WidgetBuilder,
+///     BuildContext, UiNode,
+/// };
+///
+/// fn create_always_centered_game_menu(ctx: &mut BuildContext) -> Handle<UiNode> {
+///     // Screen widget will provide current screen size to its Grid widget as a layout constraint,
+///     // thus making it fit to the current screen bounds.
+///     ScreenBuilder::new(
+///         WidgetBuilder::new().with_child(
+///             GridBuilder::new(
+///                 WidgetBuilder::new()
+///                     .with_width(300.0)
+///                     .with_height(400.0)
+///                     .with_child(
+///                         // Buttons will be stacked one on top of another.
+///                         StackPanelBuilder::new(
+///                             WidgetBuilder::new()
+///                                 .on_row(1)
+///                                 .on_column(1)
+///                                 .with_child(
+///                                     ButtonBuilder::new(WidgetBuilder::new())
+///                                         .with_text("New Game")
+///                                         .build(ctx),
+///                                 )
+///                                 .with_child(
+///                                     ButtonBuilder::new(WidgetBuilder::new())
+///                                         .with_text("Exit")
+///                                         .build(ctx),
+///                                 ),
+///                         )
+///                         .build(ctx),
+///                     ),
+///             )
+///             // Split the grid into 3 rows and 3 columns. The center cell contain the stack panel
+///             // instance, that basically stacks main menu buttons one on top of another. The center
+///             // cell will also be always centered in screen bounds.
+///             .add_row(Row::stretch())
+///             .add_row(Row::auto())
+///             .add_row(Row::stretch())
+///             .add_column(Column::stretch())
+///             .add_column(Column::auto())
+///             .add_column(Column::stretch())
+///             .build(ctx),
+///         ),
+///     )
+///     .build(ctx)
+/// }
+/// ```
 #[derive(Default, Clone, Visit, Reflect, Debug)]
 pub struct Screen {
     /// Base widget of the screen.
     pub widget: Widget,
+    /// Last screen size.
     #[visit(skip)]
     #[reflect(hidden)]
     pub last_screen_size: Cell<Vector2<f32>>,
@@ -66,15 +142,19 @@ impl Control for Screen {
     }
 }
 
+/// Screen builder creates instances of [`Screen`] widgets and adds them to the user interface.
 pub struct ScreenBuilder {
     widget_builder: WidgetBuilder,
 }
 
 impl ScreenBuilder {
+    /// Creates a new instance of the screen builder.
     pub fn new(widget_builder: WidgetBuilder) -> Self {
         Self { widget_builder }
     }
 
+    /// Finishes building a [`Screen`] widget instance and adds it to the user interface, returning a
+    /// handle to the instance.
     pub fn build(self, ui: &mut BuildContext) -> Handle<UiNode> {
         let screen = Screen {
             widget: self.widget_builder.build(),
