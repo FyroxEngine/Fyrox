c4d9d3842fb0d817d53c639626033429d873a1ff
Dmitry Stepanov <d1maxa@yandex.ru>
2024-02-10 11:25:56 +0300

    command with dynamic dispatch

--- editor/src/command/mod.rs
+++ editor/src/command/mod.rs
@@ -1,5 +1,12 @@
 use crate::scene::commands::GameSceneContext;
-use std::fmt::Debug;
+use fyrox::{
+    core::reflect::{is_path_to_array_element, Reflect, ResolvePath, SetFieldByPathError},
+    gui::inspector::{PropertyAction, PropertyChanged},
+};
+use std::{
+    any::{type_name, Any},
+    fmt::{Debug, Formatter},
+};
 
 pub mod panel;
 pub mod universal;
@@ -125,3 +132,473 @@ define_command_stack!(
     GameSceneCommandStack,
     GameSceneContext
 );
+
+pub trait BaseCommandContext: Any {
+    fn as_any(&self) -> &dyn Any;
+    fn as_any_mut(&mut self) -> &mut dyn Any;
+}
+
+impl<T: CommandContext> BaseCommandContext for T {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+}
+
+pub trait CommandContext: BaseCommandContext {}
+
+impl dyn CommandContext {
+    pub fn downcast<T>(&self) -> Option<&T>
+    where
+        T: CommandContext,
+    {
+        self.as_any().downcast_ref()
+    }
+
+    pub fn downcast_mut<T>(&mut self) -> Option<&mut T>
+    where
+        T: CommandContext,
+    {
+        self.as_any_mut().downcast_mut()
+    }
+
+    pub fn get<T: CommandContext>(&self) -> &T {
+        self.downcast().unwrap_or_else(|| {
+            panic!(
+                "Unable to downcast command context to {} type",
+                type_name::<T>()
+            )
+        })
+    }
+
+    pub fn get_mut<T: CommandContext>(&mut self) -> &mut T {
+        self.downcast_mut().unwrap_or_else(|| {
+            panic!(
+                "Unable to downcast command context to {} type",
+                type_name::<T>()
+            )
+        })
+    }
+}
+
+pub trait Command: Debug {
+    fn name(&mut self, context: &dyn CommandContext) -> String;
+    fn execute(&mut self, context: &mut dyn CommandContext);
+    fn revert(&mut self, context: &mut dyn CommandContext);
+    fn finalize(&mut self, _: &mut dyn CommandContext) {}
+}
+
+pub struct CommandStack {
+    pub commands: Vec<Box<dyn Command>>,
+    pub top: Option<usize>,
+    debug: bool,
+}
+
+impl CommandStack {
+    pub fn new(debug: bool) -> Self {
+        Self {
+            commands: Default::default(),
+            top: None,
+            debug,
+        }
+    }
+
+    pub fn do_command(&mut self, mut command: Box<dyn Command>, context: &mut dyn CommandContext) {
+        if self.commands.is_empty() {
+            self.top = Some(0);
+        } else {
+            // Advance top
+            match self.top.as_mut() {
+                None => self.top = Some(0),
+                Some(top) => *top += 1,
+            }
+            // Drop everything after top.
+            let top = self.top.unwrap_or(0);
+            if top < self.commands.len() {
+                for mut dropped_command in self.commands.drain(top..) {
+                    if self.debug {
+                        println!("Finalizing command {:?}", dropped_command);
+                    }
+                    dropped_command.finalize(context);
+                }
+            }
+        }
+
+        if self.debug {
+            println!("Executing command {:?}", command);
+        }
+
+        command.execute(context);
+
+        self.commands.push(command);
+    }
+
+    pub fn undo(&mut self, context: &mut dyn CommandContext) {
+        if !self.commands.is_empty() {
+            if let Some(top) = self.top.as_mut() {
+                if let Some(command) = self.commands.get_mut(*top) {
+                    if self.debug {
+                        println!("Undo command {:?}", command);
+                    }
+                    command.revert(context)
+                }
+                if *top == 0 {
+                    self.top = None;
+                } else {
+                    *top -= 1;
+                }
+            }
+        }
+    }
+
+    pub fn redo(&mut self, context: &mut dyn CommandContext) {
+        if !self.commands.is_empty() {
+            let command = match self.top.as_mut() {
+                None => {
+                    self.top = Some(0);
+                    self.commands.first_mut()
+                }
+                Some(top) => {
+                    let last = self.commands.len() - 1;
+                    if *top < last {
+                        *top += 1;
+                        self.commands.get_mut(*top)
+                    } else {
+                        None
+                    }
+                }
+            };
+
+            if let Some(command) = command {
+                if self.debug {
+                    println!("Redo command {:?}", command);
+                }
+                command.execute(context)
+            }
+        }
+    }
+
+    pub fn clear(&mut self, context: &mut dyn CommandContext) {
+        for mut dropped_command in self.commands.drain(..) {
+            if self.debug {
+                println!("Finalizing command {:?}", dropped_command);
+            }
+            dropped_command.finalize(context);
+        }
+    }
+}
+
+pub fn make_command<F>(
+    property_changed: &PropertyChanged,
+    entity_getter: F,
+) -> Option<Box<dyn Command>>
+where
+    F: 'static + FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    match PropertyAction::from_field_kind(&property_changed.value) {
+        PropertyAction::Modify { value } => Some(Box::new(SetPropertyCommand::new(
+            property_changed.path(),
+            value,
+            entity_getter,
+        ))),
+        PropertyAction::AddItem { value } => Some(Box::new(AddCollectionItemCommand::new(
+            property_changed.path(),
+            value,
+            entity_getter,
+        ))),
+        PropertyAction::RemoveItem { index } => Some(Box::new(RemoveCollectionItemCommand::new(
+            property_changed.path(),
+            index,
+            entity_getter,
+        ))),
+        // Must be handled outside, there is not enough context and it near to impossible to create universal reversion
+        // for InheritableVariable<T>.
+        PropertyAction::Revert => None,
+    }
+}
+
+fn try_modify_property<F>(entity: &mut dyn Reflect, path: &str, func: F)
+where
+    F: FnOnce(&mut dyn Reflect),
+{
+    let mut func = Some(func);
+    entity.resolve_path_mut(path, &mut |result| match result {
+        Ok(field) => func.take().unwrap()(field),
+        Err(e) => fyrox::core::log::Log::err(format!(
+            "There is no such property {}! Reason: {:?}",
+            path, e
+        )),
+    })
+}
+
+pub struct SetPropertyCommand<F>
+where
+    F: FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    value: Option<Box<dyn Reflect>>,
+    path: String,
+    entity_getter: F,
+}
+
+impl<F> Debug for SetPropertyCommand<F>
+where
+    F: FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "SetPropertyCommand")
+    }
+}
+
+impl<F> SetPropertyCommand<F>
+where
+    F: FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    pub fn new(path: String, value: Box<dyn Reflect>, entity_getter: F) -> Self {
+        Self {
+            value: Some(value),
+            path,
+            entity_getter,
+        }
+    }
+
+    fn swap(&mut self, ctx: &mut dyn CommandContext) {
+        if is_path_to_array_element(&self.path) {
+            (self.entity_getter)(ctx).resolve_path_mut(&self.path, &mut |result| match result {
+                Err(reason) => {
+                    fyrox::core::log::Log::err(format!(
+                        "Failed to set property {}! Invalid path {:?}!",
+                        self.path, reason
+                    ));
+                }
+                Ok(property) => match property.set(self.value.take().unwrap()) {
+                    Ok(old_value) => {
+                        self.value = Some(old_value);
+                    }
+                    Err(current_value) => {
+                        fyrox::core::log::Log::err(format!(
+                            "Failed to set property {}! Incompatible types {}!",
+                            self.path,
+                            current_value.type_name()
+                        ));
+                        self.value = Some(current_value);
+                    }
+                },
+            });
+        } else {
+            (self.entity_getter)(ctx).set_field_by_path(
+                &self.path,
+                self.value.take().unwrap(),
+                &mut |result| match result {
+                    Ok(old_value) => {
+                        self.value = Some(old_value);
+                    }
+                    Err(result) => {
+                        let value = match result {
+                            SetFieldByPathError::InvalidPath { value, reason } => {
+                                fyrox::core::log::Log::err(format!(
+                                    "Failed to set property {}! Invalid path {:?}!",
+                                    self.path, reason
+                                ));
+
+                                value
+                            }
+                            SetFieldByPathError::InvalidValue(value) => {
+                                fyrox::core::log::Log::err(format!(
+                                    "Failed to set property {}! Incompatible types {}!",
+                                    self.path,
+                                    value.type_name()
+                                ));
+
+                                value
+                            }
+                        };
+                        self.value = Some(value);
+                    }
+                },
+            );
+        }
+    }
+}
+
+impl<F> Command for SetPropertyCommand<F>
+where
+    F: FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    fn name(&mut self, _: &dyn CommandContext) -> String {
+        format!("Set {} property", self.path)
+    }
+
+    fn execute(&mut self, ctx: &mut dyn CommandContext) {
+        self.swap(ctx);
+    }
+
+    fn revert(&mut self, ctx: &mut dyn CommandContext) {
+        self.swap(ctx);
+    }
+}
+
+pub struct AddCollectionItemCommand<F>
+where
+    F: FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    path: String,
+    item: Option<Box<dyn Reflect>>,
+    entity_getter: F,
+}
+
+impl<F> Debug for AddCollectionItemCommand<F>
+where
+    F: FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "AddCollectionItemCommand")
+    }
+}
+
+impl<F> AddCollectionItemCommand<F>
+where
+    F: FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    pub fn new(path: String, item: Box<dyn Reflect>, entity_getter: F) -> Self {
+        Self {
+            path,
+            item: Some(item),
+            entity_getter,
+        }
+    }
+}
+
+impl<F> Command for AddCollectionItemCommand<F>
+where
+    F: FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    fn name(&mut self, _: &dyn CommandContext) -> String {
+        format!("Add item to {} collection", self.path)
+    }
+
+    fn execute(&mut self, ctx: &mut dyn CommandContext) {
+        try_modify_property((self.entity_getter)(ctx), &self.path, |field| {
+            field.as_list_mut(&mut |result| {
+                if let Some(list) = result {
+                    if let Err(item) = list.reflect_push(self.item.take().unwrap()) {
+                        fyrox::core::log::Log::err(format!(
+                            "Failed to push item to {} collection. Type mismatch {} and {}!",
+                            self.path,
+                            item.type_name(),
+                            list.type_name()
+                        ));
+                        self.item = Some(item);
+                    }
+                } else {
+                    fyrox::core::log::Log::err(format!(
+                        "Property {} is not a collection!",
+                        self.path
+                    ))
+                }
+            });
+        })
+    }
+
+    fn revert(&mut self, ctx: &mut dyn CommandContext) {
+        try_modify_property((self.entity_getter)(ctx), &self.path, |field| {
+            field.as_list_mut(&mut |result| {
+                if let Some(list) = result {
+                    if let Some(item) = list.reflect_pop() {
+                        self.item = Some(item);
+                    } else {
+                        fyrox::core::log::Log::err(format!(
+                            "Failed to pop item from {} collection!",
+                            self.path
+                        ))
+                    }
+                } else {
+                    fyrox::core::log::Log::err(format!(
+                        "Property {} is not a collection!",
+                        self.path
+                    ))
+                }
+            });
+        })
+    }
+}
+
+pub struct RemoveCollectionItemCommand<F>
+where
+    F: FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    path: String,
+    index: usize,
+    value: Option<Box<dyn Reflect>>,
+    entity_getter: F,
+}
+
+impl<F> Debug for RemoveCollectionItemCommand<F>
+where
+    F: FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "RemoveCollectionItemCommand")
+    }
+}
+
+impl<F> RemoveCollectionItemCommand<F>
+where
+    F: FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    pub fn new(path: String, index: usize, entity_getter: F) -> Self {
+        Self {
+            path,
+            index,
+            value: None,
+            entity_getter,
+        }
+    }
+}
+
+impl<F> Command for RemoveCollectionItemCommand<F>
+where
+    F: FnMut(&mut dyn CommandContext) -> &mut dyn Reflect,
+{
+    fn name(&mut self, _: &dyn CommandContext) -> String {
+        format!("Remove collection {} item {}", self.path, self.index)
+    }
+
+    fn execute(&mut self, ctx: &mut dyn CommandContext) {
+        try_modify_property((self.entity_getter)(ctx), &self.path, |field| {
+            field.as_list_mut(&mut |result| {
+                if let Some(list) = result {
+                    self.value = list.reflect_remove(self.index);
+                } else {
+                    fyrox::core::log::Log::err(format!(
+                        "Property {} is not a collection!",
+                        self.path
+                    ))
+                }
+            })
+        })
+    }
+
+    fn revert(&mut self, ctx: &mut dyn CommandContext) {
+        try_modify_property((self.entity_getter)(ctx), &self.path, |field| {
+            field.as_list_mut(&mut |result| {
+                if let Some(list) = result {
+                    if let Err(item) = list.reflect_insert(self.index, self.value.take().unwrap()) {
+                        self.value = Some(item);
+                        fyrox::core::log::Log::err(format!(
+                            "Failed to insert item to {} collection. Type mismatch!",
+                            self.path
+                        ))
+                    }
+                } else {
+                    fyrox::core::log::Log::err(format!(
+                        "Property {} is not a collection!",
+                        self.path
+                    ))
+                }
+            });
+        })
+    }
+}
