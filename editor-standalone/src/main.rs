6e2cff50f25b218b432506610aa655171021d207
Jesper Persson <jesper.92.persson@gmail.com>
2024-02-17 17:49:42 +0100

    Add project dir and scenes to open as cli args to editor

--- editor-standalone/src/main.rs
+++ editor-standalone/src/main.rs
@@ -1,6 +1,34 @@
+use clap::Parser;
 use fyrox::event_loop::EventLoop;
-use fyroxed_base::Editor;
+use fyroxed_base::{Editor, StartupData};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Project root directory
+    #[arg(short, long)]
+    project_directory: Option<String>,
+
+    /// List of scenes to load
+    #[arg(short, long)]
+    scenes: Option<Vec<String>>,
+}
 
 fn main() {
-    Editor::new(None).run(EventLoop::new().unwrap())
+    let args = Args::parse();
+    let startup_data = if let Some(proj_dir) = args.project_directory {
+        Some(StartupData {
+            working_directory: proj_dir.into(),
+            scenes: args
+                .scenes
+                .unwrap_or_default()
+                .iter()
+                .map(Into::into)
+                .collect(),
+        })
+    } else {
+        None
+    };
+
+    Editor::new(startup_data).run(EventLoop::new().unwrap())
 }
