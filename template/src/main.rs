41a1f1ec2cd261ae9baaa39fb94666cc14ac4fb7
Dmitry Stepanov <d1maxa@yandex.ru>
2024-03-31 17:52:34 +0300

    fixed android project template

--- template/src/main.rs
+++ template/src/main.rs
@@ -563,7 +563,7 @@ fn android_main(app: fyrox::platform::android::activity::AndroidApp) {{
     io::ANDROID_APP
         .set(app.clone())
         .expect("ANDROID_APP cannot be set twice.");
-    let event_loop = EventLoopBuilder::new().with_android_app(app).build();
+    let event_loop = EventLoopBuilder::new().with_android_app(app).build().unwrap();
     let mut executor = Executor::from_params(event_loop, Default::default());
     executor.add_plugin(Game::default());
     executor.run()
