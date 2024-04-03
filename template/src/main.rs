f9d0bb3aa1b4959e584eee65fb56c2f4e8a0e4df
Dmitry Stepanov <d1maxa@yandex.ru>
2024-04-03 15:03:53 +0300

    fixed path

--- template/src/main.rs
+++ template/src/main.rs
@@ -591,7 +591,7 @@ fn android_main(app: fyrox::platform::android::activity::AndroidApp) {{
         base_path.join("executor-android/release.keystore"),
         include_bytes!("android/release.keystore"),
     );
-    fs::create_dir_all("android/assets").unwrap();
+    fs::create_dir_all("executor-android/assets").unwrap();
 }
 
 fn init_workspace(base_path: &Path, vcs: &str) {
