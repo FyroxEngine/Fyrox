ae5946431c41074ac49e1bab140a859f467a82fc
Dmitry Stepanov <d1maxa@yandex.ru>
2024-05-05 21:42:52 +0300

    fixed frustum culling flag usage

--- fyrox-impl/src/scene/dim2/rectangle.rs
+++ fyrox-impl/src/scene/dim2/rectangle.rs
@@ -282,9 +282,10 @@ impl NodeTrait for Rectangle {
     fn collect_render_data(&self, ctx: &mut RenderContext) -> RdcControlFlow {
         if !self.global_visibility()
             || !self.is_globally_enabled()
-            || !ctx
-                .frustum
-                .map_or(true, |f| f.is_intersects_aabb(&self.world_bounding_box()))
+            || (self.frustum_culling()
+                && !ctx
+                    .frustum
+                    .map_or(true, |f| f.is_intersects_aabb(&self.world_bounding_box())))
         {
             return RdcControlFlow::Continue;
         }
