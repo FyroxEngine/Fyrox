cf50ce1c6151c6e394481916216d1d04a0668ebd
Dmitry Stepanov <d1maxa@yandex.ru>
2024-11-14 18:59:38 +0300

    useful macros for early return statements
    - while let-else exists, it still takes more lines of code than it should. these macros are much more compact and easier to read

---
+++ fyrox-core/src/early.rs
@@ -0,0 +1,91 @@
+// Copyright (c) 2019-present Dmitry Stepanov and Fyrox Engine contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#[macro_export]
+macro_rules! some_or_return {
+    ($expr:expr) => {{
+        if let Some(v) = $expr {
+            v
+        } else {
+            return;
+        }
+    }};
+    ($expr:expr, $default:expr) => {{
+        if let Some(v) = $expr {
+            v
+        } else {
+            return $default;
+        }
+    }};
+}
+
+#[macro_export]
+macro_rules! ok_or_return {
+    ($expr:expr) => {{
+        if let Ok(v) = $expr {
+            v
+        } else {
+            return;
+        }
+    }};
+    ($expr:expr, $default:expr) => {{
+        if let Ok(v) = $expr {
+            v
+        } else {
+            return $default;
+        }
+    }};
+}
+
+#[macro_export]
+macro_rules! some_or_continue {
+    ($expr:expr) => {{
+        if let Some(v) = $expr {
+            v
+        } else {
+            continue;
+        }
+    }};
+    ($expr:expr, $lifetime:lifetime) => {{
+        if let Some(v) = $expr {
+            v
+        } else {
+            continue $lifetime;
+        }
+    }};
+}
+
+#[macro_export]
+macro_rules! ok_or_continue {
+    ($expr:expr) => {{
+        if let Ok(v) = $expr {
+            v
+        } else {
+            continue;
+        }
+    }};
+    ($expr:expr, $lifetime:lifetime) => {{
+        if let Ok(v) = $expr {
+            v
+        } else {
+            continue $lifetime;
+        }
+    }};
+}
