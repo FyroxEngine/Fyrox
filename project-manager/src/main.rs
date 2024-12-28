efde2c5e649d1b0887aa3b04425d6d5715085c0c
Dmitry Stepanov <d1maxa@yandex.ru>
2024-12-28 23:59:00 +0300

    removed update rate stabilization for project manager+proper throttling
    - there's no need to stabilize the rate
    - update rate is now properly reduced during the build

--- project-manager/src/main.rs
+++ project-manager/src/main.rs
@@ -105,20 +105,24 @@ fn main() {
     let event_loop = EventLoop::new().unwrap();
 
     let mut previous = Instant::now();
-    let fixed_time_step = 1.0 / 60.0;
-    let mut lag = 0.0;
+
+    #[allow(unused_assignments)]
+    let mut time_step = 1.0 / 60.0;
 
     event_loop
         .run(move |event, window_target| {
             if project_manager.mode.is_build() {
                 // Keep updating with reduced rate to keep printing to the build log, but do not
                 // eat as much time as in normal update mode.
+                time_step = 1.0 / 10.0;
+
                 window_target.set_control_flow(ControlFlow::wait_duration(
-                    Duration::from_secs_f32(1.0 / 10.0),
+                    Duration::from_secs_f32(time_step),
                 ));
             } else {
                 // Wait for an event.
                 window_target.set_control_flow(ControlFlow::Wait);
+                time_step = 1.0 / 60.0;
             }
 
             match event {
@@ -142,10 +146,10 @@ fn main() {
                 Event::AboutToWait => {
                     let ui = engine.user_interfaces.first_mut();
 
-                    if project_manager.is_active(ui) {
-                        let elapsed = previous.elapsed();
+                    let elapsed = previous.elapsed();
+
+                    if project_manager.is_active(ui) && elapsed.as_secs_f32() >= time_step {
                         previous = Instant::now();
-                        lag += elapsed.as_secs_f32();
 
                         let mut processed = 0;
                         while let Some(message) = ui.poll_message() {
@@ -158,18 +162,9 @@ fn main() {
                                 .request_update_in_next_frame();
                         }
 
-                        while lag >= fixed_time_step {
-                            engine.update(
-                                fixed_time_step,
-                                window_target,
-                                &mut lag,
-                                Default::default(),
-                            );
+                        engine.update(time_step, window_target, &mut 0.0, Default::default());
 
-                            project_manager.update(engine.user_interfaces.first_mut());
-
-                            lag -= fixed_time_step;
-                        }
+                        project_manager.update(engine.user_interfaces.first_mut());
 
                         if let GraphicsContext::Initialized(ref ctx) = engine.graphics_context {
                             let window = &ctx.window;
