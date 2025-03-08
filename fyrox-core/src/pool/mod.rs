031046da3ed4be6376872e566071c71a3ce412f5
Dmitry Stepanov <d1maxa@yandex.ru>
2025-03-08 23:25:16 +0300

    experimental `BorrowAs` trait

--- fyrox-core/src/pool/mod.rs
+++ fyrox-core/src/pool/mod.rs
@@ -74,6 +74,11 @@ where
     free_stack: Vec<u32>,
 }
 
+pub trait BorrowAs<Object, Container: PayloadContainer<Element = Object>, T> {
+    fn borrow_as_ref(self, pool: &Pool<Object, Container>) -> Option<&T>;
+    fn borrow_as_mut(self, pool: &mut Pool<Object, Container>) -> Option<&mut T>;
+}
+
 impl<T, P> Reflect for Pool<T, P>
 where
     T: Reflect,
@@ -361,6 +366,16 @@ where
     fn records_get_mut(&mut self, index: u32) -> Option<&mut PoolRecord<T, P>> {
         let index = usize::try_from(index).expect("Index overflowed usize");
         self.records.get_mut(index)
+    }
+
+    #[inline]
+    pub fn typed_ref<Ref>(&self, handle: impl BorrowAs<T, P, Ref>) -> Option<&Ref> {
+        handle.borrow_as_ref(self)
+    }
+
+    #[inline]
+    pub fn typed_mut<Ref>(&mut self, handle: impl BorrowAs<T, P, Ref>) -> Option<&mut Ref> {
+        handle.borrow_as_mut(self)
     }
 
     #[inline]
