a570d6f710eade7920a5680d9917b8a43da2ffe9
Dmitry Stepanov <d1maxa@yandex.ru>
2025-03-27 23:23:32 +0300

    fixed warnings on wasm

--- fyrox-impl/src/engine/mod.rs
+++ fyrox-impl/src/engine/mod.rs
@@ -107,7 +107,6 @@ use fxhash::{FxHashMap, FxHashSet};
 use fyrox_animation::AnimationTracksData;
 use fyrox_graphics::gl::server::GlGraphicsServer;
 use fyrox_graphics::server::SharedGraphicsServer;
-use fyrox_resource::registry::ResourceRegistry;
 use fyrox_sound::{
     buffer::{loader::SoundBufferLoader, SoundBuffer},
     renderer::hrtf::{HrirSphereLoader, HrirSphereResourceData},
@@ -1409,6 +1408,7 @@ impl Engine {
 
         #[cfg(not(target_arch = "wasm32"))]
         {
+            use fyrox_resource::registry::ResourceRegistry;
             let rm_state = resource_manager.state();
             let io = rm_state.resource_io.clone();
             let loaders = rm_state.loaders.clone();
