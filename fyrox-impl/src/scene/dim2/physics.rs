62c6df6453421bc1e21577179c306244c6399cbc
b-guild <bguildjava@gmail.com>
2025-03-31 23:46:21 -0700

    Adding support for kinematic physics

--- fyrox-impl/src/scene/dim2/physics.rs
+++ fyrox-impl/src/scene/dim2/physics.rs
@@ -879,7 +879,7 @@ impl PhysicsWorld {
     ) {
         if *self.enabled {
             if let Some(native) = self.bodies.get(rigid_body.native.get()) {
-                if native.body_type() == RigidBodyType::Dynamic {
+                if native.body_type() != RigidBodyType::Fixed {
                     let local_transform: Matrix4<f32> = parent_transform
                         .try_inverse()
                         .unwrap_or_else(Matrix4::identity)
@@ -1015,6 +1015,15 @@ impl PhysicsWorld {
                                 native.apply_impulse_at_point(impulse, Point2::from(point), false)
                             }
                             ApplyAction::WakeUp => native.wake_up(true),
+                            ApplyAction::NextTranslation(position) => {
+                                native.set_next_kinematic_translation(position)
+                            }
+                            ApplyAction::NextRotation(rotation) => {
+                                native.set_next_kinematic_rotation(rotation)
+                            }
+                            ApplyAction::NextPosition(position) => {
+                                native.set_next_kinematic_position(position)
+                            }
                         }
                     }
                 }
