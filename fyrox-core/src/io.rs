22aadd6d1ecd453b84df7a7670a8e3959d0f0ae7
Dmitry Stepanov <d1maxa@yandex.ru>
2025-07-01 21:02:22 +0300

    `impl Display for FileError`

--- fyrox-core/src/io.rs
+++ fyrox-core/src/io.rs
@@ -18,12 +18,26 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+use std::fmt::{Display, Formatter};
 use std::{io::Error, path::Path};
 
 #[derive(Debug)]
 pub enum FileError {
     Io(std::io::Error),
     Custom(String),
+}
+
+impl Display for FileError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FileError::Io(err) => {
+                write!(f, "Io error: {}", err)
+            }
+            FileError::Custom(err) => {
+                write!(f, "{err}")
+            }
+        }
+    }
 }
 
 impl From<std::io::Error> for FileError {
