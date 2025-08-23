c755be96880b44c30d54d1cf3684e6bb44fc41c3
Dmitry Stepanov <d1maxa@yandex.ru>
2025-08-23 10:17:32 +0300

    less bright icons for scene nodes

--- editor/src/world/item.rs
+++ editor/src/world/item.rs
@@ -320,7 +320,8 @@ impl SceneItemBuilder {
                             .with_height(16.0)
                             .on_column(0)
                             .with_margin(Thickness::left_right(1.0))
-                            .with_visibility(self.icon.is_some()),
+                            .with_visibility(self.icon.is_some())
+                            .with_background(ctx.style.property(Style::BRUSH_TEXT)),
                     )
                     .with_opt_texture(self.icon)
                     .build(ctx),
