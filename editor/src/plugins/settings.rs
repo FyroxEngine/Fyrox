08733c1a24a4b28b6ff88ec1b96709d16147ee0b
Dmitry Stepanov <d1maxa@yandex.ru>
2025-09-05 20:44:10 +0300

    increased window size and inspector name column width in settings

--- editor/src/plugins/settings.rs
+++ editor/src/plugins/settings.rs
@@ -177,7 +177,7 @@ impl SettingsWindow {
         .add_row(Row::stretch())
         .build(ctx);
 
-        let window = WindowBuilder::new(WidgetBuilder::new().with_width(500.0).with_height(600.0))
+        let window = WindowBuilder::new(WidgetBuilder::new().with_width(700.0).with_height(800.0))
             .open(false)
             .with_title(WindowTitle::text("Settings"))
             .with_tab_label("Settings")
@@ -270,7 +270,7 @@ impl SettingsWindow {
             layer_index: 0,
             generate_property_string_values: true,
             filter: Default::default(),
-            name_column_width: 150.0,
+            name_column_width: 250.0,
             base_path: Default::default(),
             has_parent_object: false,
         });
