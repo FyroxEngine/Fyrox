0bd8edbde11629351a2fa92a368ebcae1f3cc6be
Dmitry Stepanov <d1maxa@yandex.ru>
2025-09-05 18:59:17 +0300

    log window improvements + fixes
    - fixed content menu not working
    - fixed empty space after the message
    - replaced list box with simple stack panel, which improved performance
    - added margins to improve readability

--- fyrox-ui/src/log.rs
+++ fyrox-ui/src/log.rs
@@ -22,14 +22,16 @@ use crate::{
     border::BorderBuilder,
     button::ButtonMessage,
     copypasta::ClipboardProvider,
+    core::{
+        log::{LogMessage, MessageKind},
+        pool::Handle,
+    },
     dropdown_list::{DropdownListBuilder, DropdownListMessage},
-    formatted_text::WrapMode,
     grid::{Column, GridBuilder, Row},
-    list_view::{ListView, ListViewBuilder, ListViewMessage},
     menu::{ContextMenuBuilder, MenuItemBuilder, MenuItemContent, MenuItemMessage},
     message::{MessageDirection, UiMessage},
     popup::{Placement, PopupBuilder, PopupMessage},
-    scroll_viewer::ScrollViewerBuilder,
+    scroll_viewer::{ScrollViewerBuilder, ScrollViewerMessage},
     stack_panel::StackPanelBuilder,
     style::{resource::StyleResourceExt, Style},
     text::{Text, TextBuilder},
@@ -37,13 +39,9 @@ use crate::{
     widget::{WidgetBuilder, WidgetMessage},
     window::{WindowBuilder, WindowMessage, WindowTitle},
     BuildContext, HorizontalAlignment, Orientation, RcUiNodeHandle, Thickness, UiNode,
-    UserInterface,
+    UserInterface, VerticalAlignment,
 };
-use fyrox_core::{
-    log::{LogMessage, MessageKind},
-    pool::Handle,
-};
-use fyrox_graph::BaseSceneGraph;
+use fyrox_graph::{BaseSceneGraph, SceneGraphNode};
 use fyrox_texture::TextureResource;
 use std::sync::mpsc::Receiver;
 
@@ -172,24 +170,24 @@ impl LogPanel {
                         .with_orientation(Orientation::Horizontal)
                         .build(ctx),
                     )
-                    .with_child({
-                        messages = ListViewBuilder::new(
+                    .with_child(
+                        ScrollViewerBuilder::new(
                             WidgetBuilder::new()
-                                .with_margin(Thickness::uniform(1.0))
                                 .on_row(1)
-                                .on_column(0),
+                                .on_column(0)
+                                .with_margin(Thickness::uniform(3.0)),
                         )
-                        .with_scroll_viewer(
-                            ScrollViewerBuilder::new(
-                                WidgetBuilder::new().with_margin(Thickness::uniform(3.0)),
+                        .with_content({
+                            messages = StackPanelBuilder::new(
+                                WidgetBuilder::new().with_margin(Thickness::uniform(1.0)),
                             )
-                            .with_horizontal_scroll_allowed(true)
-                            .with_vertical_scroll_allowed(true)
-                            .build(ctx),
-                        )
-                        .build(ctx);
-                        messages
-                    }),
+                            .build(ctx);
+                            messages
+                        })
+                        .with_horizontal_scroll_allowed(true)
+                        .with_vertical_scroll_allowed(true)
+                        .build(ctx),
+                    ),
             )
             .add_row(Row::strict(26.0))
             .add_row(Row::stretch())
@@ -242,7 +240,7 @@ impl LogPanel {
     pub fn handle_ui_message(&mut self, message: &UiMessage, ui: &mut UserInterface) {
         if let Some(ButtonMessage::Click) = message.data::<ButtonMessage>() {
             if message.destination() == self.clear {
-                ui.send_message(ListViewMessage::items(
+                ui.send_message(WidgetMessage::replace_children(
                     self.messages,
                     MessageDirection::ToWidget,
                     vec![],
@@ -267,11 +265,7 @@ impl LogPanel {
     }
 
     pub fn update(&mut self, max_log_entries: usize, ui: &mut UserInterface) -> bool {
-        let existing_items = ui
-            .node(self.messages)
-            .cast::<ListView>()
-            .map(|v| v.items())
-            .unwrap();
+        let existing_items = ui.node(self.messages).children();
 
         let mut count = existing_items.len();
 
@@ -283,11 +277,7 @@ impl LogPanel {
             // TODO: This is suboptimal, because it creates a message per each excessive entry, which
             //  might be slow to process in case of large amount of messages.
             for item in existing_items.iter().take(delta) {
-                ui.send_message(ListViewMessage::remove_item(
-                    self.messages,
-                    MessageDirection::ToWidget,
-                    *item,
-                ));
+                ui.send_message(WidgetMessage::remove(*item, MessageDirection::ToWidget));
             }
 
             count -= delta;
@@ -305,7 +295,12 @@ impl LogPanel {
             self.message_count += 1;
             received_anything = true;
 
-            let text = format!("[{:.2}s] {}", msg.time.as_secs_f32(), msg.content);
+            let mut text = format!("[{:.2}s] {}", msg.time.as_secs_f32(), msg.content);
+            if let Some(ch) = text.chars().last() {
+                if ch == '\n' {
+                    text.pop();
+                }
+            }
 
             let ctx = &mut ui.build_ctx();
             let item = BorderBuilder::new(
@@ -319,7 +314,7 @@ impl LogPanel {
                         TextBuilder::new(
                             WidgetBuilder::new()
                                 .with_context_menu(self.context_menu.menu.clone())
-                                .with_margin(Thickness::uniform(1.0))
+                                .with_margin(Thickness::uniform(2.0))
                                 .with_foreground(match msg.kind {
                                     MessageKind::Information => {
                                         ctx.style.property(Style::BRUSH_INFORMATION)
@@ -330,17 +325,17 @@ impl LogPanel {
                                     MessageKind::Error => ctx.style.property(Style::BRUSH_ERROR),
                                 }),
                         )
+                        .with_vertical_text_alignment(VerticalAlignment::Center)
                         .with_text(text)
-                        .with_wrap(WrapMode::Word)
                         .build(ctx),
                     ),
             )
             .build(ctx);
 
-            ui.send_message(ListViewMessage::add_item(
-                self.messages,
-                MessageDirection::ToWidget,
+            ui.send_message(WidgetMessage::link(
                 item,
+                MessageDirection::ToWidget,
+                self.messages,
             ));
 
             item_to_bring_into_view = item;
@@ -349,7 +344,7 @@ impl LogPanel {
         }
 
         if item_to_bring_into_view.is_some() {
-            ui.send_message(ListViewMessage::bring_item_into_view(
+            ui.send_message(ScrollViewerMessage::bring_into_view(
                 self.messages,
                 MessageDirection::ToWidget,
                 item_to_bring_into_view,
