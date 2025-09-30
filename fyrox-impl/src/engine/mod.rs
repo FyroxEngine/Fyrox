32b131a89698d46c99514c6be12f1313f046c294
LuoZheng2002 <98627375+LuoZheng2002@users.noreply.github.com>
2025-09-30 04:13:57 -0700

    added pure color texture (#845)

--- fyrox-impl/src/engine/mod.rs
+++ fyrox-impl/src/engine/mod.rs
@@ -1307,6 +1307,7 @@ pub(crate) fn initialize_resource_manager_loaders(
     state.built_in_resources.add(BUILT_IN_FONT.clone());
 
     state.built_in_resources.add(texture::PLACEHOLDER.clone());
+    state.built_in_resources.add(texture::PURE_COLOR.clone());
     state.built_in_resources.add(style::DEFAULT_STYLE.clone());
 
     for material in [
