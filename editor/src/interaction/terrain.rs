5796d3810d30c65ff5944ae7d10c33b15e772f33
Dmitry Stepanov <xcoolmrdimas@gmail.com>
2026-02-28 15:18:38 +0300

    cargo fmt

--- editor/src/interaction/terrain.rs
+++ editor/src/interaction/terrain.rs
@@ -675,9 +675,13 @@ impl BrushPanel {
         if let Some(InspectorMessage::PropertyChanged(msg)) =
             message.data_from::<InspectorMessage>(self.inspector)
         {
-            PropertyAction::from_field_action(&msg.action).apply(&msg.path(), brush, &mut |result| {
-                Log::verify(result);
-            });
+            PropertyAction::from_field_action(&msg.action).apply(
+                &msg.path(),
+                brush,
+                &mut |result| {
+                    Log::verify(result);
+                },
+            );
         }
         Some(())
     }
