c22029b6dadb05525dd0a7f5762dd8e76cb8eb5f
Dmitry Stepanov <d1maxa@yandex.ru>
2026-03-05 23:57:40 +0300

    removed forgotten `dbg!`

--- editor/src/asset/mod.rs
+++ editor/src/asset/mod.rs
@@ -899,7 +899,6 @@ impl AssetBrowser {
     }
 
     pub fn locate_path(&mut self, ui: &UserInterface, path: PathBuf) {
-        dbg!(&path);
         let folder = path.parent().map(|p| p.to_path_buf()).unwrap_or_default();
         ui.send(self.folder_browser, FileBrowserMessage::Path(folder));
         self.item_to_select = Some(path);
